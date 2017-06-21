76790c876403807dd8a2f187ac8d45c20dbd9bc7
gfranko <gfranko5@yahoo.com>
2017-06-21 11:39:57 -0700

    Add cache busting for the site CSS

---
+++ _plugins/cachebuster.rb
@@ -0,0 +1,13 @@
+# /_plugins/cachebuster.rb
+require 'digest/sha1'
+
+module Jekyll
+  class CacheBuster < Liquid::Tag
+
+    def render(context)
+      "#{Digest::SHA1.hexdigest(Time.now.to_s)}"
+    end
+  end
+end
+
+Liquid::Template.register_tag('bust_cache', Jekyll::CacheBuster)
