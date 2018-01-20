1925f9d32ccab128f01197dfccd01ce326df4ad5
StanFaas <stanfaas@gmail.com>
2018-01-20 15:53:40 +0100

    Removing ignored files

--- .next/bundles/pages/about.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/about', function() {
-            var comp = module.exports=webpackJsonp([6],{191:function(e,t,u){e.exports=u(192)},192:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var l=u(2),a=n(l),o=u(68),r=n(o);t.default=function(){return a.default.createElement(r.default,null,a.default.createElement("p",null,"This is the about page"))}}},[191]);
-            return { page: comp.default }
-          })
-        
