75ee64247a1cce07c731c66492953acebef5a91f
Edwin de Boer <edwinb276@gmail.com>
2018-01-21 02:02:40 +0100

    Move stylesheets to the correct file

--- components/Layout/Layout.js
+++ components/Layout/Layout.js
@@ -1,8 +1,16 @@
+import Head from 'next/head'
+
 import Header from '../Header/Header';
 import Footer from '../Footer/Footer';
 
 const Layout = (props) => (
   <div>
+    <Head>
+      <link rel="stylesheet" href="static/bootstrap-reboot.css" type="text/css" />
+      <link rel="stylesheet" href="static/responsive-display.css" type="text/css" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css" />
+    </Head>
+
     <Header />
 
     <div className="container">
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,7 @@
 import Layout from '../components/Layout/Layout';
-import Head from 'next/head'
 
 const Index = (props) => (
-  <Layout>
-    <Head>
-      <link rel="stylesheet" href="static/responsive-display.css" type="text/css" />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css" />
-    </Head>
-  </Layout>
+  <Layout></Layout>
 )
 
 export default Index
