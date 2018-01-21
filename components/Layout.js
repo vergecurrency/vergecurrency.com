0a74627393fb999a13963a0acde533344ee749c3
Edwin de Boer <edwinb276@gmail.com>
2018-01-21 14:10:41 +0100

    Change notation of function to support props again

--- components/Layout.js
+++ components/Layout.js
@@ -6,35 +6,39 @@ import Footer from './Footer';
 import { translate } from 'react-i18next';
 import i18n from '../i18n';
 
-const Layout = ({ t, initialI18nStore }, props) => (
-  <div>
-    <Head>
-      <link rel="stylesheet" href="static/bootstrap-reboot.css" type="text/css" />
-      <link rel="stylesheet" href="static/responsive-display.css" type="text/css" />
-      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css" />
-    </Head>
+const Layout = function (props) {
+  const { t } = props;
 
-    <Header t={t} />
-    
-    <div className="container">
-      {props.children}
+  return (
+    <div>
+      <Head>
+        <link rel="stylesheet" href="static/bootstrap-reboot.css" type="text/css" />
+        <link rel="stylesheet" href="static/responsive-display.css" type="text/css" />
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/flexboxgrid/6.3.1/flexboxgrid.min.css" type="text/css" />
+      </Head>
+
+      <Header t={t} />
+
+      <div className="container">
+        {props.children}
+      </div>
+
+      <Footer t={t} />
+
+      <style jsx>{`
+        div:not(.container) {
+          display: flex;
+          flex-direction: column;
+          min-height: 100vh;
+        }
+
+        .container {
+          flex-grow: 1;
+        }
+      `}</style>
     </div>
-
-    <Footer t={t} />
-
-    <style jsx>{`
-      div:not(.container) {
-        display: flex
-        flex-direction: column
-        min-height: 100vh
-      }
-
-      .container {
-        flex-grow: 1
-      }
-    `}</style>
-  </div>
-);
+  )
+};
 
 const Extended = translate(['header', 'footer', 'common'], { i18n, wait: process.browser })(Layout);
 
