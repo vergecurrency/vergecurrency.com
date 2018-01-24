4a90362a755a904bb8cf51a92196086fd7db6b7f
StanFaas <stanfaas@gmail.com>
2018-01-24 21:44:48 +0100

    Nav scroll event unmount working

--- components/Navbar.js
+++ components/Navbar.js
@@ -2,30 +2,31 @@ import React from 'react';
 import Router from 'next/router';
 
 export default class Navbar extends React.Component {
+
+  scrollEvent = () => {
+    const head = document.getElementById("header");
+    head.className = window.scrollY > 10 ? 'scroll' : '';
+  }
+
   componentDidMount() {
     switch (Router.route) {
       case "/":
       case "/presskit": {
-        const head = document.getElementById("header");
-        window.addEventListener('scroll', () => {
-          head.className = window.scrollY > 10 ? 'scroll' : '';
-        }, true);
+        window.addEventListener('scroll', this.scrollEvent, true);
 
         break;
       }
       default: {
         const head = document.getElementById("header");
         head.className = 'scroll';
-        
+
         break;
       }
     }
   }
 
   componentWillUnmount() {
-    window.removeEventListener('scroll', () => {
-      head.className = window.scrollY > 10 ? 'scroll' : '';
-    });
+    window.removeEventListener('scroll', this.scrollEvent, true);
   }
 
   render() {
