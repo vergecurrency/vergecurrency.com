6b8abaf558782c321fd0708cae59c7262f7b0c45
StanFaas <stanfaas@gmail.com>
2018-02-05 22:00:42 +0100

    Added vendor initial page

---
+++ pages/vendors.js
@@ -0,0 +1,13 @@
+import Layout from '../components/Layout';
+import Link from 'next/link';
+
+const Vendors = () => (
+  <Layout>
+    <div className="vendors">
+      <h1>Vendors</h1>
+      <p>Bonjour you can be a Vergendor too.</p>
+    </div>
+  </Layout>
+)
+
+export default Vendors;
