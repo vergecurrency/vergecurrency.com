f84d6e4629ab88a56c9149d3cdfe9b9a58dc8e0d
StanFaas <stanfaas@gmail.com>
2018-02-10 11:57:16 +0100

    Added wallets to the homepage

--- pages/index.js
+++ pages/index.js
@@ -122,14 +122,77 @@ const Home = function (props) {
             </div>
           </div>
           <div className="row">
-            <div className="col-md-3">
-              Tor Android Wallet
+            <div className="col-md-3 wallets--item">
+              <a href="#">
+                <div className="wallets--icon wallets--icon__android wallets--icon__android--tor"></div>
+                <div className="wallets--text">
+                  <h4>Tor Android Wallet</h4>
+                  <span>Download here</span>
+                </div>
+              </a>
             </div>
-            <div className="col-md-3">
-              OSX Electrum Wallet
+            <div className="col-md-3 wallets--item">
+              <a href="#">
+                <div className="wallets--icon wallets--icon__apple"></div>
+                <div className="wallets--text">
+                  <h4>OSX Electrum Wallet</h4>
+                  <span>Download here</span>
+                </div>
+              </a>
             </div>
-            <div className="col-md-3">
-              Apple iOS Wallet
+            <div className="col-md-3 wallets--item wallets--item__disabled">
+              <a href="#">
+                <div className="wallets--icon wallets--icon__apple"></div>
+                <div className="wallets--text">
+                  <h4>Apple iOS Wallet</h4>
+                  <span>Available soon!</span>
+                </div>
+              </a>
+            </div>
+            <div className="col-md-3 wallets--item">
+              <a href="#">
+                <div className="wallets--icon wallets--icon__apple wallets--icon__apple--tor"></div>
+                <div className="wallets--text">
+                  <h4>OSX Tor Electrum Wallet</h4>
+                  <span>Download here</span>
+                </div>
+              </a>
+            </div>
+            <div className="col-md-3 wallets--item">
+              <a href="#">
+                <div className="wallets--icon wallets--icon__windows"></div>
+                <div className="wallets--text">
+                  <h4>Windows Electrum Wallet</h4>
+                  <span>Download here</span>
+                </div>
+              </a>
+            </div>
+            <div className="col-md-3 wallets--item">
+              <a href="#">
+                <div className="wallets--icon wallets--icon__windows wallets--icon__windows--tor"></div>
+                <div className="wallets--text">
+                  <h4>Windows Tor Wallet</h4>
+                  <span>Download here</span>
+                </div>
+              </a>
+            </div>
+            <div className="col-md-3 wallets--item">
+              <a href="#">
+                <div className="wallets--icon wallets--icon__linux"></div>
+                <div className="wallets--text">
+                  <h4>Linux Electrum Wallet</h4>
+                  <span>Download here</span>
+                </div>
+              </a>
+            </div>
+            <div className="col-md-3 wallets--item">
+              <a href="#">
+                <div className="wallets--icon wallets--icon__linux wallets--icon__linux--tor"></div>
+                <div className="wallets--text">
+                  <h4>Linux Tor Electrum Wallet</h4>
+                  <span>Download here</span>
+                </div>
+              </a>
             </div>
           </div>
         </div>
