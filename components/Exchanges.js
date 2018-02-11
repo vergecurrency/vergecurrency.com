009a760a0345bde1ee721c6d3f4aad18a8a02aa8
StanFaas <stanfaas@gmail.com>
2018-02-11 14:03:21 +0100

    Cleaned up Exchanges.js code

--- components/Exchanges.js
+++ components/Exchanges.js
@@ -1,15 +1,13 @@
 import Link from 'next/link';
 
-const theThing = require('../locales/index');
-
-console.log("Logging translations", theThing);
+const exchangeLocale = require('../locales/index').en.exchanges.exchanges;
 
 const Exchanges = function (props) {
 
   let exchanges = [];
   let key = 0;
   
-  theThing.en.exchanges.exchanges.map(x => {
+  exchangeLocale.map(x => {
     exchanges.push(
       <div className="col-xs col-md-3" key={key}>
        <div className="exchanges--item">
@@ -26,23 +24,6 @@ const Exchanges = function (props) {
     )
     key += 1;
   })
-
-  // for (let i = 1; i < 6; i++){
-  //   exchanges.push(
-  //     <div className="col-xs col-md-3" key={i}>
-  //       <div className="exchanges--item">
-  //         <div className="exchanges--item__logo">
-  //           {/* TODO: Keep this clean. Translations aren't meant for this.. */ }
-  //           <img src={t("home:exchanges.exchange_" + i + ".img")} width={t("home:exchanges.exchange_" + i + ".img_width")} />
-  //         </div>
-  //         <div className="exchanges--item__name">
-  //           <span>{t("home:exchanges.exchange_" + i + ".title")}</span>
-  //           <span>{t("home:exchanges.exchange_" + i + ".link")}</span>
-  //         </div>
-  //       </div>
-  //     </div>
-  //   );
-  // }
 
   return (
     <div className="row">
