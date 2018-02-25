edc7a774454b4e008a68bb327fc82f99a7a514ef
EdwinB89 <edwinb276@gmail.com>
2018-02-25 15:40:53 +0100

    roadmap on home, now roadmap page is next

--- pages/index.js
+++ pages/index.js
@@ -218,34 +218,42 @@ function Home(props) {
               </div>
             </div>
 
-            <div className="roadmap">
-              <div className="row pt pb">
-                <div className="col-sm-8 col-sm-offset-4">
+            <div className="container themed-container--linen themed-container--linen--home">
+              <div className="row center-xs roadmap">
+                <div className="col-xs-11 col-sm-8 col-lg-6 start-xs center-lg">
                   <h2>
                     A roadmap with great features to come.<br />
                     Join us while we&apos;re still growing!
                   </h2>
-                  <ul className="roadmap--timeline">
-                    <li className="done">
-                      <h3>&quot;Black&quot; Paper v3.0</h3>
-                      <span>Released</span> 4 June 2017
-                    </li>
-                    <li className="done">
-                      <h3>Test servers deployed</h3>
-                      <span>Released</span> 10 Aug 2017
-                    </li>
-                    <li className="planned">
-                      <h3>I2P Android Wallet</h3>
-                      Test servers <br />
-                      Anonymous mobile transactions over the I2P network
-                    </li>
-                  </ul>
+                  <div className="row start-xs center-sm">
+                    <div className="col-xs-10 col-xs-offset-2 col-sm-12 col-sm-offset-0 col-md-12 col-lg-9 start-xs">
+                      <ul className="roadmap--timeline">
+                        <li className="done">
+                          <h3>&quot;Black&quot; Paper v3.0</h3>
+                          <span>Released</span> 4 June 2017
+                        </li>
+                        <li className="done">
+                          <h3>Core Wallet 3.0 Release Stage 1</h3>
+                          Wallet UI Overhaul, VISP, Bloom Filters, Atomic Swaps Capability<br />
+                          <span>Released</span> 4 June 2017
+                        </li>
+                        <li className="planned">
+                          <h3>I2P Android Wallet</h3>
+                          Anonymous mobile transactions over the I2P network
+                        </li>
+                        <li className="planned">
+                          <h3>Mining Update</h3>
+                          XVGui Miner, Official Mining Pool, Mining Guide
+                        </li>
+                      </ul>
 
-                  <h6>
-                    <Link href="/roadmap">
-                      <a href="/roadmap" className="spaced pt">Full roadmap here</a>
-                    </Link>
-                  </h6>
+                      <h6>
+                        <Link href="/roadmap">
+                          <a href="/roadmap">Full roadmap here</a>
+                        </Link>
+                      </h6>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
