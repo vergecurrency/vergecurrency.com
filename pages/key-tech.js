42941cb0a23b5bea154a39db3a44508b11fd9243
EdwinB89 <edwinb276@gmail.com>
2018-02-26 23:55:01 +0100

    #7 responsiveness and graphics

--- pages/key-tech.js
+++ pages/key-tech.js
@@ -32,9 +32,9 @@ function KeyTech(props) {
         </div>
 
         <div className="container pb-lg bb">
-          <div className="themed-container--dark themed-container--dark--wraith">
+          <div className="themed-container--dark themed-container--dark--wraith wraith">
             <div className="row center-xs">
-              <div className="col-xs-10 col-sm-6 col-md-5 col-lg-4 start-xs">
+              <div className="col-xs-10 col-sm-6 col-md-5 start-xs">
                 <h2>Wraith Protocol</h2>
                 <p>
                   Wraith Protocol is a technology that allows the user to seamlessly switch between public and private
@@ -52,80 +52,95 @@ function KeyTech(props) {
                   <a className="btn btn-primary">Read about Wraith on our blog</a>
                 </Link>
               </div>
-              <div className="col-sm-4 col-md-5 col-lg-6 hidden-xs">
-                <div className="wraith--imgs hidden-xs">
-                  ..
-                </div>
-              </div>
+              <div className="col-sm-4 col-md-5 hidden-xs" />
             </div>
           </div>
           <div className="key-features content">
-            <h6>Key features</h6>
-            <h2>
-              Low fees, quick transactions, high volume in circulation, multiplatform support, Wraith protocol are the
-              ingredients that make Verge perfectly positioned for mass adoption.
-            </h2>
-            <p>Transaction speeds are ultra-fast compared to other coins.</p>
-
-            <div className="row">
-              <div className="col-xs-12 col-sm-6">
-                <h3>Tor IP obfuscation</h3>
-                <p>
-                  Tor, derived from an acronym for the original software project name “The Onion Router”, is an IP
-                  obfuscation service which enables anonymous communication across a layered circuit based network.
-                </p>
-                <p>
-                  Verge integrates Tor in its Core, Electrum and Android wallets, thereby eliminating any single point
-                  at which the communicating peers can be determined through network surveillance that relies upon
-                  knowing its source and destination.
-                </p>
+            <div className="row center-xs">
+              <div className="col-xs-10 col-sm-8">
+                <h6>Key features</h6>
+                <h2>
+                  Low fees, quick transactions, high volume in circulation, multiplatform support, Wraith protocol are the
+                  ingredients that make Verge perfectly positioned for mass adoption.
+                </h2>
+                <p>Transaction speeds are ultra-fast compared to other coins.</p>
               </div>
-              <div className="col-xs-12 col-sm-6">
-                <h3>I2P tunneling</h3>
-                <p>
-                  I2P is a highly obfuscated tunneling service using ipv6 that anonymizes all Verge data being sent over
-                  the network.
-                </p>
-                <p>
-                  As Verge moves forward, I2P is becoming one of the key technologies ensuring anonymity of the users.
-                  As Verge moves forward, I2P is becoming one of the key technologies ensuring anonymity of the users.
-                </p>
-                <p>
-                  As Verge moves forward, I2P is becoming one of the key technologies ensuring anonymity of the users.
-                </p>
-              </div>
-              <div className="col-xs-12 col-sm-6">
-                <h3>Atomic Swaps</h3>
-                <p>
-                  Atomic Swaps, or atomic cross-chain trading, is a technology that enables direct peer-to-peer trading
-                  across separate blockchains with BIP65 support and eliminates the need for trusted third parties.
-                </p>
-                <p>
-                  Traditional centralized exchanges can be replaced by decentralized exchanges (DEX), which means you
-                  always keep your funds in your wallet and do not rely on an intermediary. By enabling Atomic Swaps
-                  capability, Verge takes a proactive step towards a decentralized future.
-                </p>
-              </div>
-              <div className="col-xs-12 col-sm-6">
-                <h3>Multi-algorithm Mining</h3>
-                <p>
-                  Verge Currency uses Proof of Work (POW) mining principle. It is one of the few cryptocurrencies to
-                  have a multi-algorithm support.
-                </p>
-                <p>
-                  Verge has 5 different hash functions: Scrypt, X17, Lyra2rev2, myr-groestl and blake2s.  Such design
-                  allows for a more equal access. Verge is not a private company funded by pre-mined coins or
-                  ICO&apos;s.
-                </p>
-                <p>
-                  Therefore allowing our community to mine Verge with good technology is vital.
-                </p>
+            </div>
+            <div className="row center-xs">
+              <div className="col-xs-10">
+                <div className="row start-xs">
+                  <div className="col-xs-12 col-sm-6 feature__item">
+                    <div className="feature__image feature__tor">
+                      <img className="img-responsive feature__image" src="static/img/key-tech/Tor.png" />
+                    </div>
+                    <h3>Tor IP obfuscation</h3>
+                    <p>
+                      Tor, derived from an acronym for the original software project name “The Onion Router”, is an IP
+                      obfuscation service which enables anonymous communication across a layered circuit based network.
+                    </p>
+                    <p>
+                      Verge integrates Tor in its Core, Electrum and Android wallets, thereby eliminating any single point
+                      at which the communicating peers can be determined through network surveillance that relies upon
+                      knowing its source and destination.
+                    </p>
+                  </div>
+                  <div className="col-xs-12 col-sm-6 feature__item">
+                    <div className="feature__image feature__i2p">
+                      <img className="img-responsive feature__image" src="static/img/key-tech/I2P.png" />
+                    </div>
+                    <h3>I2P tunneling</h3>
+                    <p>
+                      I2P is a highly obfuscated tunneling service using ipv6 that anonymizes all Verge data being sent over
+                      the network.
+                    </p>
+                    <p>
+                      As Verge moves forward, I2P is becoming one of the key technologies ensuring anonymity of the users.
+                      As Verge moves forward, I2P is becoming one of the key technologies ensuring anonymity of the users.
+                    </p>
+                    <p>
+                      As Verge moves forward, I2P is becoming one of the key technologies ensuring anonymity of the users.
+                    </p>
+                  </div>
+                  <div className="col-xs-12 col-sm-6 feature__item">
+                    <div className="feature__image feature__atomic">
+                      <img className="img-responsive feature__image" src="static/img/key-tech/Atomic.png" />
+                    </div>
+                    <h3>Atomic Swaps</h3>
+                    <p>
+                      Atomic Swaps, or atomic cross-chain trading, is a technology that enables direct peer-to-peer trading
+                      across separate blockchains with BIP65 support and eliminates the need for trusted third parties.
+                    </p>
+                    <p>
+                      Traditional centralized exchanges can be replaced by decentralized exchanges (DEX), which means you
+                      always keep your funds in your wallet and do not rely on an intermediary. By enabling Atomic Swaps
+                      capability, Verge takes a proactive step towards a decentralized future.
+                    </p>
+                  </div>
+                  <div className="col-xs-12 col-sm-6 feature__item">
+                    <div className="feature__image feature__mining">
+                      <img className="img-responsive feature__image" src="static/img/key-tech/Verge.png" />
+                    </div>
+                    <h3>Multi-algorithm Mining</h3>
+                    <p>
+                      Verge Currency uses Proof of Work (POW) mining principle. It is one of the few cryptocurrencies to
+                      have a multi-algorithm support.
+                    </p>
+                    <p>
+                      Verge has 5 different hash functions: Scrypt, X17, Lyra2rev2, myr-groestl and blake2s.  Such design
+                      allows for a more equal access. Verge is not a private company funded by pre-mined coins or
+                      ICO&apos;s.
+                    </p>
+                    <p>
+                      Therefore allowing our community to mine Verge with good technology is vital.
+                    </p>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
 
           <div className="themed-container--blue themed-container--blue--integrations">
-            <div className="row center-xs">
+            <div className="row center-xs integrations">
               <div className="col-xs-10 col-sm-6 col-md-5 col-lg-4 start-xs">
                 <h6>Integrations</h6>
                 <h2>Powering real world mainstream adoption with P2P integrations</h2>
@@ -140,9 +155,7 @@ function KeyTech(props) {
                 </p>
               </div>
               <div className="col-sm-4 col-md-5 col-lg-6 hidden-xs">
-                <div className="integrations--imgs hidden-xs">
-                  ..
-                </div>
+                <div className="integrations--imgs" />
               </div>
             </div>
           </div>
