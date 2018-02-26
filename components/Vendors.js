f3c7d5c915054610ca33917cdf728bd9cd35cb1f
StanFaas <stanfaas@gmail.com>
2018-02-26 17:39:32 +0100

    Refactored the vendors component

--- components/Vendors.js
+++ components/Vendors.js
@@ -1,17 +1,15 @@
+import Link from 'next/link';
+
 const vendorsLocale = require('../locales/index').en.vendors.vendors;
 
 function Vendors() {
   const vendors = vendorsLocale.map(x => (
-    <div className="col-xs col-md-3" key={x.title}>
-      <div className="vendors--item">
-        <div className="vendors--item__logo">
-          <img src={x.img} width="40" alt="img" />
-        </div>
-        <div className="vendors--item__name">
-          <a href={x.url} target="_blank"><span>{x.title}</span></a>
-          <span>{x.link}</span>
-        </div>
-      </div>
+    <div className="col-xs-12 col-sm-4 col-md center-xs middle-xs col--full-height pb-xs" key={x.title}>
+      <Link href={x.url}>
+        <a href={x.url} className="vendors--url">
+          <img src={x.img} width="100" alt="img" />
+        </a>
+      </Link>
     </div>
   ));
 
