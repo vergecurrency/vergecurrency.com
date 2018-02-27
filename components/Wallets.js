a47193039b342b8f8153e2e07e4964874a48e94f
StanFaas <stanfaas@gmail.com>
2018-02-27 13:20:04 +0100

    Fixed wallets bug

--- components/Wallets.js
+++ components/Wallets.js
@@ -1,20 +1,25 @@
 const walletsLocale = require('../locales/index').en.wallets.wallets;
 
 function Wallets() {
-  const wallets = walletsLocale.map(x => (
-    <div
-      className={`col-xs-12 col-sm-6 col-md-4 wallets--item start-xs ${x.available ? '' : 'wallets--item__disabled'}`}
-      key={x.name}
-    >
-      <a href={x.url}>
-        <div className={`wallets--icon ${x.classNames}`} />
-        <div className="wallets--text">
-          <h4>{x.name}</h4>
-          <span>{x.available ? 'Download here' : 'Available soon!'}</span>
+  const wallets = walletsLocale.map((x, i) => {
+    if (i > 0) {
+      return (
+        <div
+          className={`col-xs-12 col-sm-6 col-md-4 wallets--item start-xs ${x.available ? '' : 'wallets--item__disabled'}`}
+          key={x.name}
+        >
+          <a href={x.url}>
+            <div className={`wallets--icon ${x.classNames}`} />
+            <div className="wallets--text">
+              <h4>{x.name}</h4>
+              <span>{x.available ? 'Download here' : 'Available soon!'}</span>
+            </div>
+          </a>
         </div>
-      </a>
-    </div>
-  ));
+      );
+    }
+    console.log('yes');
+  });
 
   return (
     <div className="row center-xs start-sm">
