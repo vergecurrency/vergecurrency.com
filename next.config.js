2dbe9a0f166e7a25470f5f044a2ab0bfed2aab70
StanFaas <stanfaas@gmail.com>
2018-03-12 11:31:54 +0100

    Removed new uglify config - it sucked

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@ const path = require('path');
 const glob = require('glob-all');
 const SWPrecacheWebpackPlugin = require('sw-precache-webpack-plugin');
 const webpack = require('webpack');
+const Uglify = require('uglifyjs-webpack-plugin');
 const BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
 
 module.exports = {
@@ -104,24 +105,50 @@ module.exports = {
     }));
     config.plugins = config.plugins.filter(plugin => (plugin.constructor.name !== 'UglifyJsPlugin'));
     config.plugins.push(new webpack.optimize.UglifyJsPlugin({
-      output: {
-        comments: false,
-      },
-      mangle: true,
-      sourcemap: false,
-      debug: false,
-      minimize: true,
-      compress: {
-        warnings: false,
-        screw_ie8: true,
-        conditionals: true,
-        unused: true,
-        comparisons: true,
-        sequences: true,
-        dead_code: true,
-        evaluate: true,
-        if_return: true,
-        join_vars: true,
+      uglifyOptions: {
+        output: {
+          comments: false,
+        },
+        exclude: [/\.min\.js$/gi], // skip pre-minified libs
+        compress: {
+          arrows: false,
+          booleans: false,
+          cascade: false,
+          collapse_vars: false,
+          comparisons: false,
+          computed_props: false,
+          hoist_funs: false,
+          hoist_props: false,
+          hoist_vars: false,
+          if_return: false,
+          inline: false,
+          join_vars: false,
+          keep_infinity: true,
+          loops: false,
+          negate_iife: false,
+          properties: false,
+          reduce_funcs: false,
+          reduce_vars: false,
+          sequences: false,
+          side_effects: false,
+          switches: false,
+          top_retain: false,
+          toplevel: false,
+          typeofs: false,
+          unused: false,
+          warnings: false, // Suppress uglification warnings
+          pure_getters: true,
+          unsafe: true,
+          unsafe_comps: true,
+          screw_ie8: true,
+          
+          // Switch off all types of compression except those needed to convince
+          // react-devtools that we're using a production build
+          conditionals: true,
+          dead_code: true,
+          evaluate: true,
+        },
+        mangle: true,
       },
     }));
     // config.plugins.push(new BundleAnalyzerPlugin());
