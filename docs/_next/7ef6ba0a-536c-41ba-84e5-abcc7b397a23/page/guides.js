311e92888ab3a758bf5067a9bcc7ee9c384e30f8
StanFaas <stanfaas@gmail.com>
2018-03-24 22:50:16 +0100

    Build folder added

---
+++ docs/_next/7ef6ba0a-536c-41ba-84e5-abcc7b397a23/page/guides.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/guides', function() {
+            var comp = module.exports=webpackJsonp([21],{347:function(e,t,l){e.exports=l(348)},348:function(e,t,l){"use strict";function a(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var u=l(0),n=a(u),d=l(2),r=a(d),c=l(9),f=a(c);t.default=function(){return n.default.createElement(r.default,null,n.default.createElement("div",{className:"learnmore"},n.default.createElement("div",{className:"container"},n.default.createElement(f.default,null,n.default.createElement("div",{className:"guides"},n.default.createElement("h1",null,"Guides"))))))}}},[347]);
+            return { page: comp.default }
+          })
+        
