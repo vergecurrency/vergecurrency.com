30f6fb4b9e325d18021dc4bca0898d9eabae8085
EdwinB89 <edwinb276@gmail.com>
2018-03-24 18:54:21 +0100

    home metadata

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Link from 'next/link';
 import LazyLoad from 'react-lazyload';
+import Head from 'next/head'
 
 import Layout from '../components/Layout';
 import { HomeExchanges } from '../components/Exchanges';
@@ -26,6 +27,10 @@ function Home(props) {
 
   return (
     <Layout>
+      <Head>
+        <title>{t('common:meta.home.title', { defaultValue: 'Verge - Secure and anonymous cryptocurrency, built with a focus on privacy' })}</title>
+        <meta name="description" content={t('common:meta.home.description', { defaultValue: 'VERGE (XVG) is a secure and anonymous cryptocurrency, built with a focus on privacy.' })} />
+      </Head>
       <div className="home">
         <div className="ribbon">
           <div className="ribbon-img" />
