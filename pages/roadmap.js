15c5a635799f7e606dfa08b888999c62ca856956
EdwinB89 <edwinb276@gmail.com>
2018-03-24 16:42:38 +0100

    #71 update roadmap with preliminary content

--- pages/roadmap.js
+++ pages/roadmap.js
@@ -28,38 +28,65 @@ const Roadmap = () => (
                     <li className="roadmap__item roadmap__item--done">
                       <h3>Core Wallet 3.0 Release Stage 1</h3>
                       Wallet UI Overhaul, VISP, Bloom Filters, Atomic Swaps Capability<br />
-                      <span>Released</span> 4 June 2017
-                    </li>
-                    <li className="roadmap__item roadmap__item--done">
-                      <h3>I2P Android Wallet</h3>
-                      Anonymous mobile transactions over the I2P network
+                      <span>Released</span> 15 November 2017
                     </li>
                     <li className="roadmap__year roadmap__year--current">
                       <span>2018</span>
                     </li>
+                    <li className="roadmap__item roadmap__item--done">
+                      <h3>Core Wallet 4.0 Release Stage 2</h3>
+                      Tor Integration & Optional Stealth Addressing<br />
+                      <span>Released</span> 1 January 2018
+                    </li>
+                    <li className="roadmap__item roadmap__item--done">
+                      <h3>New website</h3>
+                      New look, guides, updated roadmap, list of official core members, blog with official news, mobile friendly<br />
+                      <span>Released</span> X March 2018
+                      {/* TODO: Replace with correct day */}
+                    </li>
+                    <li className="roadmap__item roadmap__item--planned">
+                      <h3>Merchandise Online Store</h3>
+                      Verge branded apparel that can be purchased with XVG and BTC.<br />
+                      <b>Q1</b>
+                    </li>
                     <li className="roadmap__item roadmap__item--planned">
                       <h3>Mining Update</h3>
-                      XVGui Miner, Official Mining Pool, Mining Guide
+                      XVGui Miner, Official Mining Pool, Mining Guide<br />
+                      <b>Q1</b>
+                    </li>
+                    <li className="roadmap__item roadmap__item--planned">
+                      <h3>RSK Smart Contracts</h3>
+                      They are in testnet for Bitcoin now. Date revolves around this timeline.<br />
+                      <s>Q4 2017</s> <b>Q1</b>
+                    </li>
+                    <li className="roadmap__item roadmap__item--planned">
+                      <h3>RingCT integration</h3>
+                      <b>Q2</b>
                     </li>
                     <li className="roadmap__item roadmap__item--planned">
                       <h3>I2P Android Wallet</h3>
-                      Anonymous mobile transactions over the I2P network
+                      Anonymous mobile transactions over the I2P network<br />
+                      <b>Q2</b>
                     </li>
                     <li className="roadmap__item roadmap__item--planned">
-                      <h3>Mining Update</h3>
-                      XVGui Miner, Official Mining Pool, Mining Guide
+                      <h3>Tor I2P Electrum Wallet</h3>
+                      <b>Q3</b>
                     </li>
-                    <li className="roadmap__year roadmap__year--next">
+                    <li className="roadmap__item roadmap__item--planned">
+                      <h3>Test Tor I2P Electrum Wallet</h3>
+                      <b>Q3</b>
+                    </li>
+                    {/* <li className="roadmap__year roadmap__year--next">
                       <span>2019</span>
-                    </li>
-                    <li className="roadmap__item roadmap__item--planned">
+                      </li>
+                      <li className="roadmap__item roadmap__item--planned">
                       <h3>I2P Android Wallet</h3>
                       Anonymous mobile transactions over the I2P network
-                    </li>
-                    <li className="roadmap__item roadmap__item--planned">
+                      </li>
+                      <li className="roadmap__item roadmap__item--planned">
                       <h3>Mining Update</h3>
                       XVGui Miner, Official Mining Pool, Mining Guide
-                    </li>
+                    </li> */}
                   </ul>
                 </div>
               </div>
