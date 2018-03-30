1ec01b0736e759f74415bebc41bfa1e041eedb61
StanFaas <stanfaas@gmail.com>
2018-03-30 10:47:29 +0200

    Updates

---
+++ _next/b4ab6ff4-548d-4481-9a75-9802f085253b/page/guides/how-to-buy-verge.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/guides/how-to-buy-verge', function() {
+            var comp = module.exports=webpackJsonp([22],{344:function(e,t,l){e.exports=l(345)},345:function(e,t,l){"use strict";function a(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var u=l(0),n=a(u),r=l(2),c=a(r),d=l(9),f=a(d);t.default=function(){return n.default.createElement(c.default,null,n.default.createElement("div",{className:"learnmore"},n.default.createElement("div",{className:"container"},n.default.createElement(f.default,null,n.default.createElement("div",{className:"htbv"},n.default.createElement("h1",null,"How to buy Verge"))))))}}},[344]);
+            return { page: comp.default }
+          })
+        
---
+++ _next/b4ab6ff4-548d-4481-9a75-9802f085253b/page/guides/introduction.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/guides/introduction', function() {
+            var comp = module.exports=webpackJsonp([20],{348:function(e,t,l){e.exports=l(349)},349:function(e,t,l){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var a=l(0),u=n(a),r=l(2),c=n(r),d=l(9),o=n(d);t.default=function(){return u.default.createElement(c.default,null,u.default.createElement("div",{className:"learnmore"},u.default.createElement("div",{className:"container"},u.default.createElement(o.default,null,u.default.createElement("div",{className:"introduction"},u.default.createElement("h1",null,"This is the introduction"))))))}}},[348]);
+            return { page: comp.default }
+          })
+        
