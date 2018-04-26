a41eb13ff0ed857ba103387b6a82240e30a5a495
StanFaas <stanfaas@gmail.com>
2018-04-26 10:54:40 +0200

    Added the latest articles that we were mentioned in to the pressreleases page

--- components/Mentions.js
+++ components/Mentions.js
@@ -7,17 +7,22 @@ import 'moment-timezone';
 const mentionLocale = require('../locales/index').en.mentions.mentions;
 
 export const Mentions = () => {
-  const mentions = mentionLocale.map(x => (
-    <div className="col-xs-12 col-sm-4 col-md center-xs middle-xs col--full-height pb-xs" key={x.url}>
-      <Link href={x.url}>
-        <a href={x.url}>
-          <LazyLoad height={50}>
-            <img src={x.img} width={x.width} alt="img" />
-          </LazyLoad>
-        </a>
-      </Link>
-    </div>
-  ));
+  const mentions = mentionLocale.map((x, i) => {
+    if (i < 6) {
+      return (
+        <div className="col-xs-12 col-sm-4 col-md center-xs middle-xs col--full-height pb-xs" key={x.url}>
+          <Link href={x.url}>
+            <a href={x.url}>
+              <LazyLoad height={50}>
+                <img src={x.img} width={x.width} alt="img" />
+              </LazyLoad>
+            </a>
+          </Link>
+        </div>
+      );
+    }
+    return true;
+  });
 
   return (
     <div className="row center-xs middle-xs pt-xs">
