ed083637936ae19263b4c12781fa1e33b9db3251
StanFaas <stanfaas@gmail.com>
2018-05-01 19:02:16 +0000

    Travis CI automated build

--- _next/5fa34ee0-c2be-41eb-b069-1a117632e20c/page/guides.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/guides', function() {
-            var comp = module.exports=webpackJsonp([24],{352:function(e,t,l){e.exports=l(353)},353:function(e,t,l){"use strict";function a(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var u=l(0),n=a(u),d=l(2),r=a(d),c=l(9),f=a(c);t.default=function(){return n.default.createElement(r.default,null,n.default.createElement("div",{className:"learnmore"},n.default.createElement("div",{className:"container"},n.default.createElement(f.default,null,n.default.createElement("div",{className:"guides"},n.default.createElement("h1",null,"Guides"))))))}}},[352]);
-            return { page: comp.default }
-          })
-        
