780b8105ce4207cef2aa0e8f50a72e642d4200ea
EdwinB89 <edwinb276@gmail.com>
2018-05-06 13:02:01 +0200

    Removed <Link> from external links on Sponsors (always non-internal so no need for Link component) Added CryptoDen and HeightCare sponsor

--- components/Sponsors.js
+++ components/Sponsors.js
@@ -6,17 +6,15 @@ const sponsorLocale = require('../locales/index').en.sponsors.sponsors;
 export const Sponsors = () => {
   const Sponsors = sponsorLocale.map(s => (
     <div
-      className="col-xs col-md-3 col-lg-2 center-xs"
+      className="col-xs-4 col-sm-3 col-md-3 col-lg-2 center-xs pb-xs"
       key={s.title}
       role="presentation"
     >
-      <Link href={s.url}>
-        <a href={s.url} target="_blank" rel="noopener">
-          <LazyLoad height={s.height}>
-            <img className="img-responsive sponsors__logo" src={s.img} style={{ maxHeight: s.height }} alt={s.title} />
-          </LazyLoad>
-        </a>
-      </Link>
+      <a href={s.url} target="_blank" rel="noopener">
+        <LazyLoad height={s.height}>
+          <img className="img-responsive sponsors__logo" src={s.img} style={{ maxHeight: s.height }} alt={s.title} />
+        </LazyLoad>
+      </a>
     </div>
   ));
 
