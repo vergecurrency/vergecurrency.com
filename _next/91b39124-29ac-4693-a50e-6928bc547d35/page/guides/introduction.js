619a9a91cf5e4cae6264fe2ae12b866e8dc8bb9e
StanFaas <stanfaas@gmail.com>
2018-05-20 11:30:03 +0000

    Travis CI automated build

--- _next/91b39124-29ac-4693-a50e-6928bc547d35/page/guides/introduction.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/guides/introduction', function() {
-            var comp = module.exports=webpackJsonp([24],{355:function(e,t,l){e.exports=l(356)},356:function(e,t,l){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var a=l(0),u=n(a),r=l(2),c=n(r),d=l(9),o=n(d);t.default=function(){return u.default.createElement(c.default,null,u.default.createElement("div",{className:"learnmore"},u.default.createElement("div",{className:"container"},u.default.createElement(o.default,null,u.default.createElement("div",{className:"introduction"},u.default.createElement("h1",null,"This is the introduction"))))))}}},[355]);
-            return { page: comp.default }
-          })
-        
---
+++ _next/f33e41dd-8b22-48d8-a5b6-4d5f0d22a3aa/page/seedphrase/Mnemonic.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/seedphrase/Mnemonic', function() {
+            var comp = module.exports=webpackJsonp([48],{410:function(o,t,e){o.exports=e(411)},411:function(o,t,e){"use strict"}},[410]);
+            return { page: comp.default }
+          })
+        
