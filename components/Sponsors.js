79457ffdf7f732f59d4b857a8af5133b797238a9
StanFaas <stanfaas@gmail.com>
2018-06-03 16:18:00 +0200

    Added main sponsor and some other sponsors

--- components/Sponsors.js
+++ components/Sponsors.js
@@ -21,9 +21,25 @@ export const Sponsors = () => {
   return (
     <div className="row center-xs sponsors">
       <div className="col-xs-10 pt-small pb-small bb">
-        <div className="row middle-xs center-xs">
+        <div className="row start-xs start-xs pb">
           <div className="col-xs-12 col-sm-3 col-lg-2 end-sm">
-            <p>Event sponsors</p>
+            <p style={{ textAlign: 'left', padding: '10px 0' }}>Main sponsor</p>
+          </div>
+          <div className="col-xs">
+            <div className="row start-xs middle-xs">
+              <div className="col-xs-4 col-sm-3 center-xs pb-xs sponsors__single" role="presentation">
+                <a href="https://www.blacktowerfm.com/locations/the-netherlands" target="_blank" rel="noopener">
+                  <LazyLoad height="100">
+                    <img className="img-responsive sponsors__logo" src="../static/img/meetup/sponsors/blacktower.png" style={{ maxHeight: "100px" }} alt="Blacktower financial management group" />
+                  </LazyLoad>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+        <div className="row start-xs start-xs">
+          <div className="col-xs-12 col-sm-3 col-lg-2 end-sm">
+            <p style={{ textAlign: 'left', padding: '20px 0' }}>Event sponsors</p>
           </div>
 
           <div className="col-xs">
