7443aec1ef8afc0d15b57c14d1a1577eed567413
Jeff S <31370541+jrsikors@users.noreply.github.com>
2018-06-09 18:55:12 -0500

    Update MenuItems.js

--- components/MenuItems.js
+++ components/MenuItems.js
@@ -25,12 +25,6 @@ const MenuItems = ({ t }) => (
         {/* <Link prefetch href="/history">
           <a href="/history">{t('header:history', { defaultValue: 'History' })}</a>
         </Link> */}
-        <Link prefetch href="/presskit">
-          <a href="/presskit">{t('header:presskit', { defaultValue: 'Presskit' })}</a>
-        </Link>
-        <Link href="https://vergefora.com">
-          <a href="https://vergefora.com" target="_blank" rel="noopener noreferrer">{t('header:forums', { defaultValue: 'Forums' })}</a>
-        </Link>
         <Link prefetch href="/pressreleases">
           <a href="/pressreleases">{t('header:pressreleases', { defaultValue: 'Press Releases' })}</a>
         </Link>
@@ -39,6 +33,39 @@ const MenuItems = ({ t }) => (
     </Link>
       </div>
     </span>
+    <span className="submenu">
+      {t('header:Find Us', { defaultValue: 'Find Us' })}<FontAwesomeIcon icon={faChevronDown} />
+      <div className="submenu__content">
+        <Link href="https://www.twitter.com/vergecurrency">
+          <a href="https://www.twitter.com/vergecurrency" target="_blank" rel="noopener noreferrer">{t('header:twitter', { defaultValue: 'Twitter' })}</a>
+        </Link>
+        <Link href="https://www.reddit.com/r/vergecurrency/">
+          <a href="https://www.reddit.com/r/vergecurrency/" target="_blank" rel="noopener noreferrer">{t('header:reddit', { defaultValue: 'Reddit' })}</a>
+        </Link>
+        <Link href="https://vergefora.com">
+          <a href="https://vergefora.com" target="_blank" rel="noopener noreferrer">{t('header:forums', { defaultValue: 'Forums' })}</a>
+        </Link>		
+        <Link href="https://discord.gg/vergecurrency">
+          <a href="https://discord.gg/vergecurrency" target="_blank" rel="noopener noreferrer">{t('header:discord', { defaultValue: 'Discord' })}</a>
+        </Link>
+        <Link href="https://t.me/VERGExvg">
+          <a href="https://t.me/VERGExvg" target="_blank" rel="noopener noreferrer">{t('header:telegram', { defaultValue: 'Telegram' })}</a>
+        </Link>
+        <Link href="https://www.facebook.com/VERGEcurrency">
+          <a href="https://www.facebook.com/VERGEcurrency" target="_blank" rel="noopener noreferrer">{t('header:facebook', { defaultValue: 'FaceBook' })}</a>
+        </Link>
+        <Link href="https://www.youtube.com/channel/UCv59uw_WhHB2VxbBs0LPeeQ">
+          <a href="https://www.youtube.com/channel/UCv59uw_WhHB2VxbBs0LPeeQ" target="_blank" rel="noopener noreferrer">{t('header:youtube', { defaultValue: 'YouTube' })}</a>
+        </Link>
+        <Link href="https://github.com/vergecurrency?tab=repositories">
+          <a href="https://github.com/vergecurrency?tab=repositories" target="_blank" rel="noopener noreferrer">{t('header:github', { defaultValue: 'GitHub' })}</a>
+        </Link>
+        <Link prefetch href="/community/get-involved">
+          <a href="/community/get-involved">{t('header:contact', { defaultValue: 'Contact Direct' })}</a>
+        </Link>		
+      </div>
+    </span>
+
     <Link prefetch href="/wallets">
       <a href="/wallets">{t('header:wallets', { defaultValue: 'Wallets' })}</a>
     </Link>
@@ -60,12 +87,12 @@ const MenuItems = ({ t }) => (
         <Link prefetch href="/p2p">
           <a href="/p2p">{t('header:p2p', { defaultValue: 'P2P' })}</a>
         </Link>
-        <Link prefetch href="/community/get-involved">
-          <a href="/community/get-involved">{t('header:community', { defaultValue: 'Community' })}</a>
-        </Link>
         <Link prefetch href="/developers">
           <a href="/developers">{t('header:developers', { defaultValue: 'Developers' })}</a>
         </Link>
+        <Link prefetch href="/presskit">
+          <a href="/presskit">{t('header:presskit', { defaultValue: 'Presskit' })}</a>
+        </Link>		
       </div>
     </span>
     <Link prefetch href="/get-verge">
