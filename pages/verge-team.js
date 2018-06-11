012b115510d68082feab6dd4fa9249eb2384c5c7
Jeff S <31370541+jrsikors@users.noreply.github.com>
2018-06-11 09:34:06 -0500

    Update verge-team.js

--- pages/verge-team.js
+++ pages/verge-team.js
@@ -46,7 +46,7 @@ function VergeTeam(props) {
           <div className="row center-xs middle-xs">
             <div className="col-xs-10 col-sm-6">
               <h2>
-                Verge Currency is a 100% open source project and the global
+                Verge Currency is a 100% open-source project, and the global
                 Verge Community represents the manpower driving it forward.
                 Verge is not a company, there was no ICO held upon the launch
                 and no pre-mining took place.  All contributors, including
