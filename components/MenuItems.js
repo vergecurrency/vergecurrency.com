63ecea4e86c0b45ba61eea80140337ce083fbe99
marpme <cryzar95@googlemail.com>
2018-06-29 08:52:29 +0200

    Remove duplicates

--- components/MenuItems.js
+++ components/MenuItems.js
@@ -55,14 +55,6 @@ const MenuItems = ({ t }) => (
         <Link prefetch href="/p2p">
           <a href="/p2p">{t('header:p2p', { defaultValue: 'P2P' })}</a>
         </Link>
-        <Link prefetch href="/community/get-involved">
-          <a href="/community/get-involved">{t('header:community', { defaultValue: 'Community' })}</a>
-        </Link>
-        <Link href="https://vergefora.com">
-          <a href="https://vergefora.com" target="_blank" rel="noopener noreferrer">
-            {t('header:forums', { defaultValue: 'Forums' })}
-          </a>
-        </Link>
         <Link prefetch href="/developers">
           <a href="/developers">{t('header:developers', { defaultValue: 'Developers' })}</a>
         </Link>
