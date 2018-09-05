1e97b36805651ea3080117207b66fa9e56206db9
Jeff S <31370541+jrsikors@users.noreply.github.com>
2018-09-05 15:43:14 -0500

    Update key-tech.js

--- pages/key-tech.js
+++ pages/key-tech.js
@@ -39,6 +39,7 @@ function KeyTech(props) {
                 <h2>{t('common:wraith.title', { defaultValue: 'Wraith Protocol' })}</h2>
                 <p>
                   {t('common:wraith.text1', { defaultValue: 'Wraith Protocol is a technology upgrade package that enables our users to be able to send and receive payments privately across our blockchain by enabling stealth addressing services.' })} 
+                  &nbsp;
                   {t('common:wraith.text2', { defaultValue: 'Additionally this update removes our QT wallet users off of clearnet and migrates everyone to SSL enabled Tor.' })}
                 </p>
                 <p>
