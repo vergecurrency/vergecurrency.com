a47224d27c4d8ec510529ed7e748682bb844da20
StanFaas <stanfaas@gmail.com>
2018-09-20 16:16:12 +0000

    Travis CI automated build

--- _next/7aee6bae-f925-49e9-a648-a6a683751c45/page/seedphrase/Mnemonic.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/seedphrase/Mnemonic', function() {
-            var comp = module.exports=webpackJsonp([50],{414:function(o,t,e){o.exports=e(415)},415:function(o,t,e){"use strict"}},[414]);
-            return { page: comp.default }
-          })
-        
