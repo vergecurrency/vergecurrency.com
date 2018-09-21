0df4e9cf3eba8e9c749b5080b5a134f10a259db7
marpme <marvin.piekarek5@gmail.com>
2018-09-21 17:38:01 +0200

    Make the filtering clearer (non-exclusive)

--- components/Vendors.js
+++ components/Vendors.js
@@ -7,69 +7,78 @@ export const HomeVendors = () => {
   const vendors = vendorsLocale.map((x, i) => {
     if (i < 20) {
       return (
-        <div className="col-xs-12 col-sm-6 col-md-3 center-xs middle-xs col--full-height pb--sm" key={x.title}>
+        <div
+          className="col-xs-12 col-sm-6 col-md-3 center-xs middle-xs col--full-height pb--sm"
+          key={x.title}
+        >
           <Link href={x.url}>
-            <a href={x.url} className="vendors--url" target="_blank" rel="noopener">
+            <a
+              href={x.url}
+              className="vendors--url"
+              target="_blank"
+              rel="noopener"
+            >
               <LazyLoad height={50}>
                 <img src={x.img} width="100" alt="img" />
               </LazyLoad>
             </a>
           </Link>
-         <br /> <br />
+          <br /> <br />
         </div>
       );
     }
     return true;
   });
 
-  return (
-    <div className="row pt" >
-      {vendors}
-    </div >
-  );
+  return <div className="row pt">{vendors}</div>;
 };
 
-const checkIfFilterIsOff = (filters) => 
-  Object.values(filters).every(val => val === false)
+const checkIfFilterIsOff = filters =>
+  Object.values(filters).every(val => val === false);
 
-const filterVendorList = (vendors, filters) => 
-  checkIfFilterIsOff(filters) ? vendors : vendors.filter(
-    vendor => 
-      vendor.categories.retail === filters.retail &&
-      vendor.categories.foodAndBev === filters.foodAndBev &&
-      vendor.categories.service === filters.service &&
-      vendor.categories.entertainment === filters.entertainment &&
-      vendor.categories.travel === filters.travel &&
-      vendor.categories.education === filters.education
-  )
+const filterObject = obj => {
+  const result = {};
+  for (const key in obj) {
+    if (obj[key] === true) result[key] = true;
+  }
 
-export const LatestVendors = (categoriesFilter) => {
-  const vendors = filterVendorList(vendorsLocale, categoriesFilter)
-    .map(vendor => (
-        <div
-          className="col-xs col-md-3"
-          key={vendor.title}
-          role="presentation"
-        >
-          <a href={vendor.url} target="_blank" rel="noopener">
-            <div className="vendors--item middle-xs">
-              <div className="vendors--item__logo">
-                <LazyLoad height={40}>
-                  <img src={vendor.img} alt="img" />
-                </LazyLoad>
-              </div>
-              <div className="vendors--item__name">
-                <h4>{vendor.title}</h4>
-                <span>{vendor.link}</span>
-              </div>
+  return result;
+};
+
+const filterVendorList = (vendors, filters) => {
+  const clearedFilter = filterObject(filters);
+  if (checkIfFilterIsOff(filters)) {
+    return vendors;
+  }
+  return vendors.filter(vendor => {
+    let fits = 0;
+    for (const key in clearedFilter) {
+      if (vendor.categories[key] === clearedFilter[key]) fits += 1;
+    }
+    return fits === Object.keys(clearedFilter).length;
+  });
+};
+
+export const LatestVendors = categoriesFilter => {
+  const vendors = filterVendorList(vendorsLocale, categoriesFilter).map(
+    vendor => (
+      <div className="col-xs col-md-3" key={vendor.title} role="presentation">
+        <a href={vendor.url} target="_blank" rel="noopener">
+          <div className="vendors--item middle-xs">
+            <div className="vendors--item__logo">
+              <LazyLoad height={40}>
+                <img src={vendor.img} alt="img" />
+              </LazyLoad>
             </div>
-          </a>
-        </div>
-    ));
-
-  return (
-    <div className="row pt">
-      {vendors}
-    </div>
+            <div className="vendors--item__name">
+              <h4>{vendor.title}</h4>
+              <span>{vendor.link}</span>
+            </div>
+          </div>
+        </a>
+      </div>
+    )
   );
+
+  return <div className="row pt">{vendors}</div>;
 };
