a67e79ccc1b42e5f9c406c50e26e9e5bb25d81c7
Swen van Zanten <info@hdsign.nl>
2018-11-12 18:57:12 +0100

    Update iOS to 35% (#564)

--- pages/CurrentYearRoadmap.js
+++ pages/CurrentYearRoadmap.js
@@ -84,7 +84,7 @@ const roadMap = [
     title: 'Official iOS wallet',
     description:
       'iOS wallet with full-fledged feature set like a normal wallet.',
-    progress: 30,
+    progress: 35,
     progressState: 'development ongoing',
   },
 ];
