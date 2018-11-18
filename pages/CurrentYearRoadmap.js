8fa68f3b0f58679098e61c98e2595dcdd684dbce
Swen van Zanten <info@hdsign.nl>
2018-11-18 22:04:36 +0100

    iOS progress to 40%

--- pages/CurrentYearRoadmap.js
+++ pages/CurrentYearRoadmap.js
@@ -84,7 +84,7 @@ const roadMap = [
     title: 'Official iOS wallet',
     description:
       'iOS wallet with full-fledged feature set like a normal wallet.',
-    progress: 35,
+    progress: 40,
     progressState: 'development ongoing',
   },
 ];
