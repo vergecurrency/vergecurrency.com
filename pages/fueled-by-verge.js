13f945e7c0f666b32d512ea2c1cd607b9b936f1d
NiteShadowXVG <45295084+NiteShadowXVG@users.noreply.github.com>
2018-11-25 21:15:12 -0500

    FBV v2 - fixes and changes (#578)

--- pages/fueled-by-verge.js
+++ pages/fueled-by-verge.js
@@ -15,70 +15,129 @@ function FBV(props) {
     <Layout>
       <Head>
         <title key="title">{t('common:meta.FBV.title', { defaultValue: 'Fueled by Verge - VergeCurrency.com' })}</title>
+          <script type="text/javascript" src="myscript.js"></script>
+          <link rel="stylesheet" type="text/css" href="/static/scss/pages/_fbv.scss" />
       </Head>
 
 
-	<div class="fbvRibbon">
-	  <img src="/static/img/fbv/topBannerRibbon.png" alt="Fueled by Verge"></img>
-	  <div class="fbvRibbonHeading">Fueled by Verge</div><br />
-	  <div class="fbvRibbonHeadingSub2">Commitment to mass adoption by spreading awareness of Verge and cryptocurrencies.</div>
+
+
+
+<div class="fbv">
+
+
+
+	<div class="ribbon ribbon--fbv">
+			<div class="fbvRibbonHeading">Fueled by Verge</div><br />
+			<div class="fbvRibbonHeadingSub2">Commitment to mass adoption by spreading awareness of cryptocurrencies and Verge.</div>
+					<div class="row center-xs middle-xs">
+							<img class="fbvSticker" src="/static/img/fbv/fbv_sticker_logo.png" alt="Fueled by Verge"></img>
+							<img class="fbvBike" src="/static/img/fbv/fbv_ribbon_bike.png" alt="Motocross Bike"></img>
+				</div>
+		</div>
 	</div>
+
+
+
+
+
+
+
+	<div class="intro pt-large pb-large">
+		<div class="row center-xs middle-xs">
+			<div class="col-xs-12 col-sm-8">
+				<div class="fbvHeading">About this initiative</div>
+				<p>Fueled by Verge is a statement of commitment to mass adoption by spreading awareness of cryptocurrencies and Verge, by the VergeFam. It is a worldwide effort from individuals excelling in their unique fields and with it they will spread awareness of what Verge is all about.</p>
+				<div class="fbvButton"><a href="https://vergecurrency.com/community/send-an-email/">Join the Movement</a></div>
+			</div>
+		</div>
+	</div>
+
+
+
+
+
+
+	<div class="intro pt-large pb-large">
+		<div class="container center-xs">
+			<div class="fbvAthletesHeading">Verge Athletes</div>
+			<div class="fbv-grid">
+					<div>
+						<a href="https://www.instagram.com/141mx/" target="_blank"><img src="/static/img/fbv/dannyRobertson.png" alt="Danny Robertson Fueled by Verge"></img></a>
+					
+							<div class="fbv_react-expand-collapse__content" styles="height: 6em;">
+								<div class="fbv_react-expand-collapse__body">
+									<h3>Motosports</h3>
+									<h2>Danny Robertson</h2>
+									<h4>Calgary, Canada</h4>
+								</div>
+							</div>
+					</div>
+
+					<div>
+						<a href="https://www.instagram.com/gillinster/?hl=en" target="_blank"><img src="/static/img/fbv/gilLinster.png" alt="Gil Linster Fueled by Verge"></img></a>
+					
+							<div class="fbv_react-expand-collapse__content" styles="height: 6em;">
+								<div class="fbv_react-expand-collapse__body">
+									<h3>Racing</h3>
+									<h2>Gil Linster</h2>
+									<h4>Luxembourg</h4>
+								</div>
+							</div>
+					</div>
+
+
+					<div>
+						<a href="https://vergecurrency.com/community/send-an-email/"><img src="/static/img/fbv/yourSport.png" alt="Your Sport Fueled by Verge"></img></a>
+						
+							<div class="fbv_react-expand-collapse__content" styles="height: 6em;">
+								<div class="fbv_react-expand-collapse__body">
+									<h3>Would You Like to Join?</h3>
+									<h2>Contact Us!</h2>
+								</div>
+							</div>
+					</div>
+
+
+				</div>
+
+			</div>
+		</div>
 	
-	<div class="fbvAbout">
-	  <div class="divTableBody">
-	    <div class="divTableRow">
-		  <div class="fbvHeading">About this initiative</div>
+
+
+
+
+
+	<div class="intro pt-large pb-large">
+		<div class="row center-xs middle-xs">
+			<div class="col-xs-12 col-sm-8">
+				<div class="fbvCommunityHeading">Fueled by Verge is a community at large <br /> initiative, reflecting the main goal of Verge</div>
+				<p>It is a worldwide effort from individuals excelling in their unique fields, masters of their crafts, and to share their achievements. With this, we can build awareness and education of what Verge and blockchain is all about.</p>
+			</div>
+
+			<div class="center-xs col-xs-12 col-sm-8">
+				<div class="fbvMotoBike"></div>
+				<img src="/static/img/fbv/motocrossBike.png"></img>
+			</div>
+
+			<div class="col-xs-12 col-sm-8">
+				<p>The goal of #FueledByVerge is to showcase the utility of Verge through events. Our Verge Currency team is the first team that is fueled by Verge, in design, and spirit. <br /><br />
+
+				FbV wants to showcase the strength and the community of Cryptocurrency by helping Sponsored individuals showcase Verge. Thus bringing increased awareness of Verge Currency and cryptocurrencies in general. <br /><br />
+
+				FbV also wishes to help people - by promoting their events, and any other important information for these people. This helps them recieve supporting fans and these fans following them. With regards to this, FbV will promote their bios, as well as their respective schedules and how to find it/where to buy them.  All to increase media presence and brand awareness. </p>
+				<div class="fbvButton"><a href="https://vergecurrency.com/community/send-an-email/">Join the Movement</a></div>
+			</div>
 		</div>
-	  </div>  
-	  <div class="divTableRow">
-	    <div class="divTableCell">Fueled by Verge is a statement of commitment to mass adoption by spreading awareness of Verge and cryptocurrencies, by the VergeFam. It is a worldwide effort from individuals excelling in their unique fields and with it they will spread awareness of what XVG is all about.</div>
-	  </div>
-	  <div class="divTableRow">
-	    <div class="fbvButton"><a href="https://vergecurrency.com/community/send-an-email/">Join the Movement</a></div>
-	  </div>
 	</div>
 
-	<div class="fbvAthletes">
-	  <div class="divTableBody">
-		<div class="divTableRow">
-	 	  <div class="fbvAthletesHeading">Verge Athletes</div>
-	    </div>
-	  </div>
-	</div>
 
-	<div class="fbvAthletes">
-	  <div class="fbvAthletesRow">
-		<div class="fbvAthletesColumn"><a href="https://www.instagram.com/141mx/" target="_blank"><img src="/static/img/fbv/dannyRobertson.png" alt="Danny Robertson Fueled by Verge" styles="width:100%"></img></a></div>
-		<div class="fbvAthletesColumn"><a href="https://www.instagram.com/gillinster/?hl=en" target="_blank"><img src="/static/img/fbv/gilLinster.png" alt="Gil Linster Fueled by Verge" styles="width:100%"></img></a></div>
-		<div class="fbvAthletesColumn"><a href="https://vergecurrency.com/community/send-an-email/"><img src="/static/img/fbv/yourSport.png" alt="Your Sport Fueled by Verge" styles="width:100%"></img></a></div>
-	  </div>
-	</div>
 
-	<div class="fbvCommunity">
-	  <div class="divTableBody">
-	    <div class="divTableRow">
-	      <div class="fbvCommunityHeading">Fueled by Verge is a community at large <br /> initiative, reflecting the main goal of Verge</div>
-		</div>
-	  </div>
-	  <div class="divTableRow">
-	    <div class="divTableCell">It is a worldwide effort from individuals excelling in their unique fields, masters of their craft, to share their achievement and with it, build awareness and education of what Verge and blockchain is all about.</div>
-	  </div>
-	</div>
+<br /><br /><br />
 
-	<div class="fbvMotoBG">
-		<img src="/static/img/fbv/motocrossBike.png" alt="Motocross Fueled by Verge"></img>
-	</div>	
 
-	<div class="fbvMotoContent">
-	  <div class="divTableRow">
-	    <div class="divTableCell">The goal of #FueledByVerge is to showcase the utility of Verge through sporting events. Verge Currency is the first team that is fueled by Verge, in both design and spirit.<br /><br /> FbV wants to showcase the strength and the community of Cryptocurrency by helping Sponsored individuals showcase Verge. Thus, bringing increased awareness of Verge Currency and cryptocurrencies in general!<br /><br />FbV also wishes to help the athletes by promoting the events, and the important information for these sports people, insuring their fans can follow them. With regards to this, FbV will promote the bio of each individual, as well as their respective sporting schedules and how to find it/where to buy it. All of this culminates increased media presence of our athletes and brand awareness.</div>
-  	  </div>
-	  <div class="divTableRow">
-	    <div class="fbvButton"><a href="https://vergecurrency.com/community/send-an-email/">Join the Movement</a></div>
-	  </div>
-	</div>
 
-	<br /><br /><br />
     </Layout>
   );
 }
