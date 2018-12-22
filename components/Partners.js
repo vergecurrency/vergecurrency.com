fc4e74f412ef873613a74918a5c7d157293f5bae
marpme <marvin.piekarek5@gmail.com>
2018-12-22 19:26:18 +0100

    Adding images and texts for new parsnips

--- components/Partners.js
+++ components/Partners.js
@@ -72,6 +72,34 @@ export const PartnerInfo = ({ t }) => {
               })}
             </ExpandCollapse>
           </div>
+          <div>
+            <a href="https://www.plaak.com/">
+              <img src="/static/img/partners/plaak.png" alt="plaak" />
+            </a>
+            <ExpandCollapse {...options}>
+              {t('home:partnership.plaak', {
+                defaultValue: `PLAAK is a Singaporean based Software Development & Blockchain Technology 
+                Company with a suite of high-tech and easy to use applications for mobile and desktop 
+                computer devices. Developing useful everyday applications that can store, transact and 
+                also trade digital currencies encourages global use and wider spread adoption. 
+                One such product, is the PLAAK Core Card a sleek and easy to carry and use device, 
+                enabling Core Card owners to safely store, transact and receive digital currencies anywhere.`
+              })}
+            </ExpandCollapse>
+          </div>
+          <div>
+            <a href="https://thecryptoden.io/">
+              <img src="/static/img/partners/thecryptoden.png" alt="cryptoden" />
+            </a>
+            <ExpandCollapse {...options}>
+              {t('home:partnership.thecryptoden', {
+                defaultValue: `The Crypto Den is a multi-platform service aimed at mass adoption through education.
+                The Crypto Den's goal is being a one stop shop, to make crypto less cryptic for the public whilst facilitating 
+                a peer-to-peer crypto marketplace, as well as presenting networking and partnership opportunities between communities, 
+                projects, exchanges and businesses alike.`
+              })}
+            </ExpandCollapse>
+          </div>
         </div>
       </div>
     </div>
