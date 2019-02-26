3357006ee9e2ed733c66657d32a8f146a96a8f99
Max1us <damien@maxius.com.au>
2019-02-26 20:35:48 +1100

    Meetup Sponsor Update Add Get Started Redirect

---
+++ components/GetStartedRedirect.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default class GetStartedRedirect extends React.Component {
+  componentDidMount() {
+    window.location.replace('https://www.vergecurrency.com/get-started');
+  }
+
+  render() {
+    return (true);
+  }
+}
