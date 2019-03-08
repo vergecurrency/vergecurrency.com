e68f349d0ab16b94c2ee63f21ab9e3f7a72c10c1
Jeff S <31370541+jrsikors@users.noreply.github.com>
2019-03-07 18:03:22 -0600

    Getstarted redirect fix (#649)
    * remove get-started page, update get-verge page with link on how to buy verge with fiat
    
    * no message
    
    * update redirect

--- components/GetStartedRedirect.js
+++ components/GetStartedRedirect.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 export default class GetStartedRedirect extends React.Component {
   componentDidMount() {
-    window.location.replace('https://www.vergecurrency.com/get-started');
+    window.location.replace('https://www.vergecurrency.com/get-verge');
   }
 
   render() {
