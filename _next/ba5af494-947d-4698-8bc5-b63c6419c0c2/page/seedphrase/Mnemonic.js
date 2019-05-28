ee98aa2e05e0a4ab00fc84467ae73bfbc5de8d24
marpme <marpme@users.noreply.github.com>
2019-05-28 04:21:08 +0000

    Travis CI automated build

--- _next/ba5af494-947d-4698-8bc5-b63c6419c0c2/page/seedphrase/Mnemonic.js
+++
@@ -1,6 +0,0 @@
-
-          window.__NEXT_REGISTER_PAGE('/seedphrase/Mnemonic', function() {
-            var comp = module.exports=webpackJsonp([53],{431:function(o,t,e){o.exports=e(432)},432:function(o,t,e){"use strict"}},[431]);
-            return { page: comp.default }
-          })
-        
