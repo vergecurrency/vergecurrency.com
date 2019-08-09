e3cca55136c421c438282e6c1e92b6df26820b26
sunerok <justinvforvendetta@users.noreply.github.com>
2019-08-09 00:33:41 -0400

    more pool updates

--- pages/community/xvg-mining-pools.js
+++ pages/community/xvg-mining-pools.js
@@ -16,34 +16,26 @@ const MiningPools = () => (
               <li><Link href="https://verge-scrypt.miningpoolhub.com/"><a href="https://verge-scrypt.miningpoolhub.com/" target="_blank" rel="noopener noreferrer">[SCRYPT] MiningPoolHub.com</a></Link></li>
               {/* <li><Link href="https://xvg.thecoin.pw/index.php?page=statistics&action=pool"><a href="https://xvg.thecoin.pw/index.php?page=statistics&action=pool" target="_blank" rel="noopener noreferrer">[SCRYPT] TheCoin.pw</a></Link></li> */}
               <li><Link href="http://www.zpool.ca/site/mining"><a href="http://www.zpool.ca/site/mining" target="_blank" rel="noopener noreferrer">[SCRYPT] ZPool</a></Link></li>
-              <li><Link href="http://scrypt.ispace.co.uk/coindetails/?coin=xvg&id=79"><a href="http://scrypt.ispace.co.uk/coindetails/?coin=xvg&id=79" target="_blank" rel="noopener noreferrer">[SCRYPT] iSpace Mining Pools</a></Link></li>
               <li><Link href="https://www.mining-dutch.nl"><a href="https://www.mining-dutch.nl" target="_blank" rel="noopener noreferrer">[SCRYPT] mining-dutch.nl</a></Link></li>
               <li><Link href="http://xvg.mastermining.net/index.php?page=statistics&action=pool"><a href="http://xvg.mastermining.net/index.php?page=statistics&action=pool" target="_blank" rel="noopener noreferrer">[SCRYPT] MasterMining.net</a></Link></li>
               <li><Link href="https://www.multipool.us/dashboard/pool/xvg"><a href="https://www.multipool.us/dashboard/pool/xvg" target="_blank" rel="noopener noreferrer">[SCRYPT] Multipool.us</a></Link></li>
               <strong>Myr-Groestl</strong>
-	      		  <li><Link href="http://bsod.pw/site/gomining?algo=xvggroestl"><a href="http://bsod.pw/site/gomining?algo=xvggroestl" target="_blank" rel="noopener noreferrer">[myr-groestl] BSOD.PW</a></Link></li>
-                           <li><Link href="https://miningpoolhub.com/"><a href="https://miningpoolhub.com/" target="_blank" rel="noopener noreferrer">[myr-groestl] MiningPoolHub.com</a></Link></li>
-
-	      <li><Link href="https://hashfaster.com/"><a href="https://hashfaster.com/" target="_blank" rel="noopener noreferrer">[myr-groestl] HashFaster </a></Link></li>
+	      <li><Link href="http://bsod.pw/site/gomining?algo=xvggroestl"><a href="http://bsod.pw/site/gomining?algo=xvggroestl" target="_blank" rel="noopener noreferrer">[myr-groestl] BSOD.PW</a></Link></li>
+	      <li><Link href="http://blockmasters.co"><a href="http://blockmasters.co" target="_blank" rel="noopener noreferrer">[myr-groestl] Block Masters</a></Link></li>
               <li><Link href="http://poolovich.pro/"><a href="http://poolovich.pro/" target="_blank" rel="noopener noreferrer">[myr-groestl] Poolovich </a></Link></li>
               <li><Link href="http://xvg-mg.idcray.com/"><a href="http://xvg-mg.idcray.com/" target="_blank" rel="noopener noreferrer">[myr-groestl] IDcray </a></Link></li>
-              <li><Link href="http://www.zpool.ca/site/mining][myr-groestl"><a href="http://www.zpool.ca/site/mining][myr-groestl" target="_blank" rel="noopener noreferrer"> ZPool</a></Link></li>
-              <li><Link href="http://antminepool.com"><a href="http://antminepool.com" target="_blank" rel="noopener noreferrer">[myr-groestl] AntMinePool</a></Link></li>
 	      <li><Link href="https://www.mining-dutch.nl"><a href="https://www.mining-dutch.nl" target="_blank" rel="noopener noreferrer">[myr-groestl] mining-dutch.nl</a></Link></li>
               <strong>Lyra2Rev2</strong>
 	      		  <li><Link href="http://bsod.pw/site/gomining?algo=xvglyra"><a href="http://bsod.pw/site/gomining?algo=xvglyra" target="_blank" rel="noopener noreferrer">[Lyra2REv2] BSOD.PW</a></Link></li>
-                          <li><Link href="https://miningpoolhub.com/"><a href="https://miningpoolhub.com/" target="_blank" rel="noopener noreferrer">[Lyra2Rev2] MiningPoolHub.com</a></Link></li>
 		<li><Link href="http://blockmasters.co/"><a href="http://blockmasters.co/" target="_blank" rel="noopener noreferrer">[Lyra2Rev2] Block Masters </a></Link></li>
               <li><Link href="http://poolovich.pro/"><a href="http://poolovich.pro/" target="_blank" rel="noopener noreferrer">[Lyra2Rev2] Poolovich </a></Link></li>
               <li><Link href="https://hashfaster.com/"><a href="https://hashfaster.com/" target="_blank" rel="noopener noreferrer">[Lyra2re] HashFaster </a></Link></li>
               <li><Link href="http://xvg-lyra.idcray.com/"><a href="http://xvg-lyra.idcray.com/" target="_blank" rel="noopener noreferrer">[Lyra2re] IDcray </a></Link></li>
               <li><Link href="http://www.zpool.ca/site/mining"><a href="http://www.zpool.ca/site/mining" target="_blank" rel="noopener noreferrer">[Lyra2re] ZPool</a></Link></li>
-              <li><Link href="http://antminepool.com"><a href="http://antminepool.com" target="_blank" rel="noopener noreferrer">[Lyra2Rev2] AntMinePool</a></Link></li>
 	      <li><Link href="https://www.mining-dutch.nl"><a href="https://www.mining-dutch.nl" target="_blank" rel="noopener noreferrer">[Lyra2Re] mining-dutch.nl</a></Link></li>
               <strong>Blake2s</strong>
               <li><Link href="http://www.zpool.ca/site/mining"><a href="http://www.zpool.ca/site/mining" target="_blank" rel="noopener noreferrer">[Blake2s] ZPool</a></Link></li>
               <li><Link href="https://pool.unimining.net/site/mining"><a href="https://pool.unimining.net/site/mining" target="_blank" rel="noopener noreferrer">[Blake2s] UniMining</a></Link></li>
-              <li><Link href="http://antminepool.com"><a href="http://antminepool.com" target="_blank" rel="noopener noreferrer">[Blake2s] AntMinePool</a></Link></li>
               <li><Link href="http://verge.blake2s.com"><a href="http://verge.blake2s.com" target="_blank" rel="noopener noreferrer">[Blake2s] Verge Blake2s</a></Link></li>
               <li><Link href="http://bsod.pw/site/gomining?algo=xvg"><a href="http://bsod.pw/site/gomining?algo=xvg" target="_blank" rel="noopener noreferrer">[Blake2s] BSOD.PW</a></Link></li>
               <li><Link href="http://nlpool.nl"><a href="http://nlpool.nl" target="_blank" rel="noopener noreferrer">[Blake2s] NLpool.nl</a></Link></li>
@@ -52,14 +44,13 @@ const MiningPools = () => (
 	      <li><Link href="https://www.mining-dutch.nl"><a href="https://www.mining-dutch.nl" target="_blank" rel="noopener noreferrer">[Blake2s] mining-dutch.nl</a></Link></li>
               <strong>x17</strong>
 			        <li><Link href="http://bsod.pw/site/gomining?algo=xvgx17"><a href="http://bsod.pw/site/gomining?algo=xvgx17" target="_blank" rel="noopener noreferrer">[x17] BSOD.PW</a></Link></li>
-              <li><Link href="http://blockmasters.co/"><a href="http://blockmasters.co/" target="_blank" rel="noopener noreferrer">[x17] Block Masters </a></Link></li> 
-                      <li><Link href="https://miningpoolhub.com/"><a href="https://miningpoolhub.com/" target="_blank" rel="noopener noreferrer">[x17] MiningPoolHub.com</a></Link></li>      
+              <li><Link href="http://blockmasters.co/"><a href="http://blockmasters.co/" target="_blank" rel="noopener noreferrer">[x17] Block Masters </a></Link></li>       
 	<li><Link href="https://hashfaster.com/"><a href="https://hashfaster.com/" target="_blank" rel="noopener noreferrer">[x17] HashFaster </a></Link></li>    
               <li><Link href="http://poolovich.pro/"><a href="http://poolovich.pro/" target="_blank" rel="noopener noreferrer">[x17] Poolovich </a></Link></li>
               <li><Link href="http://yiimp.eu"><a href="http://yiimp.eu/" target="_blank" rel="noopener noreferrer">[x17] Yiimp</a></Link></li>
               <li><Link href="http://www.zpool.ca/site/mining"><a href="http://www.zpool.ca/site/mining" target="_blank" rel="noopener noreferrer">[x17] ZPool</a></Link></li>
               <li><Link href="http://nlpool.nl"><a href="http://nlpool.nl" target="_blank" rel="noopener noreferrer">[x17] NLPOOL.NL</a></Link></li>
-              <li><Link href="http://antminepool.com"><a href="http://antminepool.com" target="_blank" rel="noopener noreferrer">[x17] AntMinePool</a></Link></li>
+		<li><Link href="http://antminepool.com"><a href="http://antminepool.com" target="_blank" rel="noopener noreferrer">[x17] AntMinePool</a></Link></li>
               <li><Link href="http://verge.carpecrypto.me/"><a href="http://verge.carpecrypto.me/" target="_blank" rel="noopener noreferrer">[x17] CarpeCrypto Verge Pool</a></Link></li>
 	      <li><Link href="https://www.mining-dutch.nl"><a href="https://www.mining-dutch.nl" target="_blank" rel="noopener noreferrer">[X17] mining-dutch.nl</a></Link></li>
               <strong>Multi Algorithm (hash to verge)</strong>
