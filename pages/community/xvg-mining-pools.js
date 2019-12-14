0d2e018ae3aa4eb72649f89bbdaec7b0bd5cd4f6
sunerok <justinvforvendetta@users.noreply.github.com>
2019-12-14 08:30:34 -0500

    add zergpool

--- pages/community/xvg-mining-pools.js
+++ pages/community/xvg-mining-pools.js
@@ -19,6 +19,7 @@ const MiningPools = () => (
               <div className="pool-item"><Link href="http://xvg.mastermining.net/index.php?page=statistics&action=pool"><a href="http://xvg.mastermining.net/index.php?page=statistics&action=pool" target="_blank" rel="noopener noreferrer">MasterMining.net</a></Link></div>
               <div className="pool-item"><Link href="https://www.multipool.us/dashboard/pool/xvg"><a href="https://www.multipool.us/dashboard/pool/xvg" target="_blank" rel="noopener noreferrer">Multipool.us</a></Link></div>
               <div className="pool-item"><Link href="https://www.f2pool.com/"><a href="https://www.f2pool.com/" target="_blank" rel="noopener noreferrer">F2Pool</a></Link></div>
+              <div className="pool-item"><Link href="http://zergpool.com/site/mining?algo=scrypt"><a href="http://zergpool.com/site/mining?algo=scrypt" target="_blank" rel="noopener noreferrer">ZergPool</a></Link></div>
               <h2>Myr-Groestl</h2>
               <div className="pool-item"><Link href="https://bsod.pw/en/pool/dashboard/xvg/"><a href="https://bsod.pw/en/pool/dashboard/xvg/" target="_blank" rel="noopener noreferrer">BSOD.PW</a></Link></div>
               <div className="pool-item"><Link href="https://hashfaster.com/"><a href="https://hashfaster.com/" target="_blank" rel="noopener noreferrer">HashFaster </a></Link></div>
@@ -29,6 +30,7 @@ const MiningPools = () => (
               <div className="pool-item"><Link href="https://www.mining-dutch.nl"><a href="https://www.mining-dutch.nl" target="_blank" rel="noopener noreferrer">mining-dutch.nl</a></Link></div>
               <div className="pool-item"><Link href="https://xvg-mg.suprnova.cc/"><a href="https://xvg-mg.suprnova.cc/" target="_blank" rel="noopener noreferrer">suprnova.cc</a></Link></div>
               <div className="pool-item"><Link href="https://www.f2pool.com/"><a href="https://www.f2pool.com/" target="_blank" rel="noopener noreferrer">F2Pool</a></Link></div>
+              <div className="pool-item"><Link href="http://zergpool.com/site/mining?algo=myr-gr"><a href="http://zergpool.com/site/mining?algo=myr-gr" target="_blank" rel="noopener noreferrer">ZergPool</a></Link></div>
               <h2>Lyra2Rev2</h2>
               <div className="pool-item"><Link href="https://bsod.pw/en/pool/dashboard/xvg/"><a href="https://bsod.pw/en/pool/dashboard/xvg/" target="_blank" rel="noopener noreferrer">BSOD.PW</a></Link></div>
               <div className="pool-item"><Link href="http://blockmasters.co/"><a href="http://blockmasters.co/" target="_blank" rel="noopener noreferrer">Block Masters </a></Link></div>
@@ -40,6 +42,7 @@ const MiningPools = () => (
               <div className="pool-item"><Link href="https://www.mining-dutch.nl"><a href="https://www.mining-dutch.nl" target="_blank" rel="noopener noreferrer">mining-dutch.nl</a></Link></div>
               <div className="pool-item"><Link href="https://xvg-lyra.suprnova.cc/"><a href="https://xvg-lyra.suprnova.cc/" target="_blank" rel="noopener noreferrer">suprnova.cc</a></Link></div>
               <div className="pool-item"><Link href="https://www.f2pool.com/"><a href="https://www.f2pool.com/" target="_blank" rel="noopener noreferrer">F2Pool</a></Link></div>
+              <div className="pool-item"><Link href="http://zergpool.com/site/mining?algo=lyra2v2"><a href="http://zergpool.com/site/mining?algo=lyra2v2" target="_blank" rel="noopener noreferrer">ZergPool</a></Link></div>
               <h2>Blake2s</h2>
               <div className="pool-item"><Link href="http://www.zpool.ca/site/mining"><a href="http://www.zpool.ca/site/mining" target="_blank" rel="noopener noreferrer">ZPool</a></Link></div>
               <div className="pool-item"><Link href="https://pool.unimining.net/site/mining"><a href="https://pool.unimining.net/site/mining" target="_blank" rel="noopener noreferrer">UniMining</a></Link></div>
@@ -48,6 +51,7 @@ const MiningPools = () => (
               <div className="pool-item"><Link href="http://nlpool.nl"><a href="http://nlpool.nl" target="_blank" rel="noopener noreferrer">NLpool.nl</a></Link></div>
               <div className="pool-item"><Link href="https://www.mining-dutch.nl"><a href="https://www.mining-dutch.nl" target="_blank" rel="noopener noreferrer">mining-dutch.nl</a></Link></div>
               <div className="pool-item"><Link href="https://www.f2pool.com/"><a href="https://www.f2pool.com/" target="_blank" rel="noopener noreferrer">F2Pool</a></Link></div>
+              <div className="pool-item"><Link href="http://zergpool.com/site/mining?algo=blake2s"><a href="http://zergpool.com/site/mining?algo=blake2s" target="_blank" rel="noopener noreferrer">ZergPool</a></Link></div>
               <h2>x17</h2>
               <div className="pool-item"><Link href="https://bsod.pw/en/pool/dashboard/xvg/"><a href="https://bsod.pw/en/pool/dashboard/xvg/" target="_blank" rel="noopener noreferrer">BSOD.PW</a></Link></div>
               <div className="pool-item"><Link href="http://blockmasters.co/"><a href="http://blockmasters.co/" target="_blank" rel="noopener noreferrer">Block Masters </a></Link></div>
@@ -59,6 +63,7 @@ const MiningPools = () => (
               <div className="pool-item"><Link href="https://www.mining-dutch.nl"><a href="https://www.mining-dutch.nl" target="_blank" rel="noopener noreferrer">mining-dutch.nl</a></Link></div>
               <div className="pool-item"><Link href="https://xvg-x17.suprnova.cc/"><a href="https://xvg-x17.suprnova.cc/" target="_blank" rel="noopener noreferrer">suprnova.cc</a></Link></div>
               <div className="pool-item"><Link href="https://www.f2pool.com/"><a href="https://www.f2pool.com/" target="_blank" rel="noopener noreferrer">F2Pool</a></Link></div>
+              <div className="pool-item"><Link href="http://zergpool.com/site/mining?algo=x17"><a href="http://zergpool.com/site/mining?algo=x17" target="_blank" rel="noopener noreferrer">ZergPool</a></Link></div>
               <h2>Multi Algorithm (hash to verge)</h2>
               <div className="pool-item"><Link href="https://hash-to-coins.com/index.php"><a href="https://hash-to-coins.com/index.php" target="_blank" rel="noopener noreferrer">Hash to Coins</a></Link></div>
               <div className="pool-item"><Link href="https://prohashing.com/"><a href="https://prohashing.com/" target="_blank" rel="noopener noreferrer">ProHashing</a></Link></div>
