88f2bdbb946ff498b67b78728bfb8d1edf61044c
marpme <marpme@users.noreply.github.com>
2020-10-18 10:21:02 +0000

    Travis CI automated build

---
+++ _next/136e8106-3e52-4c0b-81c8-220970e1db39/page/getstarted.js
@@ -0,0 +1,6 @@
+
+          window.__NEXT_REGISTER_PAGE('/getstarted', function() {
+            var comp = module.exports=webpackJsonp([45],{359:function(e,t,u){e.exports=u(360)},360:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var r=u(15),o=n(r),l=u(11),f=n(l),a=u(12),d=n(a),i=u(16),c=n(i),s=u(17),p=n(s),_=u(0),v=n(_),y=u(361),h=n(y),m=function(e){function t(){return(0,f.default)(this,t),(0,c.default)(this,(t.__proto__||(0,o.default)(t)).apply(this,arguments))}return(0,p.default)(t,e),(0,d.default)(t,[{key:"render",value:function(){return v.default.createElement(h.default,null)}}]),t}(v.default.Component);t.default=m},361:function(e,t,u){"use strict";function n(e){return e&&e.__esModule?e:{default:e}}Object.defineProperty(t,"__esModule",{value:!0});var r=u(15),o=n(r),l=u(11),f=n(l),a=u(12),d=n(a),i=u(16),c=n(i),s=u(17),p=n(s),_=u(0),v=n(_),y=function(e){function t(){return(0,f.default)(this,t),(0,c.default)(this,(t.__proto__||(0,o.default)(t)).apply(this,arguments))}return(0,p.default)(t,e),(0,d.default)(t,[{key:"componentDidMount",value:function(){window.location.replace("https://www.vergecurrency.com/get-verge")}},{key:"render",value:function(){return!0}}]),t}(v.default.Component);t.default=y}},[359]);
+            return { page: comp.default }
+          })
+        
