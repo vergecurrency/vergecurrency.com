7e5d3baea8ebf931d9a97d7079d1796d2b577136
dhavald99 <dhavald99@gmail.com>
2022-11-16 14:54:01 +0530

    add time stamp in binancecnt exchange url

--- components/Exchanges.js
+++ components/Exchanges.js
@@ -1,8 +1,16 @@
 import LazyLoad from 'react-lazyload';
 import { shuffle } from './Shuffler';
 
-const exchangeLocale = require('../lists/exchanges').exchanges;
-
+const exchangeLocale = require('../lists/exchanges').exchanges
+exchangeLocale.map((x, i) => {
+  if (x.link === 'binancecnt.com') {
+    if (x.url.includes('&timestamp=')) {
+      x.url.replace(/(timestamp=)[^\&]+/, '$1' + new Date().valueOf())
+    } else {
+      x.url += '&timestamp=' + new Date().valueOf()
+    }
+  }
+});
 const shuffledExchanges = shuffle(exchangeLocale, 5);
 
 export const HomeExchanges = () => {
@@ -43,29 +51,31 @@ export const HomeExchanges = () => {
 };
 
 export const Exchanges = () => {
-  const exchanges = shuffledExchanges.map(x => (
-    <div
-      className="col-xs col-md-3"
-      key={x.title}
-      role="presentation"
-    >
-      <a>
-        <a href={x.url} target="_blank" rel="noopener noreferrer">
-          <div className="exchanges--item middle-xs">
-            <div className="exchanges--item__logo">
-              <LazyLoad height={40}>
-                <img src={x.img} alt="img" />
-              </LazyLoad>
+  const exchanges = shuffledExchanges.map(x => {
+    return (
+      <div
+        className="col-xs col-md-3"
+        key={x.title}
+        role="presentation"
+      >
+        <a>
+          <a href={x.url} target="_blank" rel="noopener noreferrer">
+            <div className="exchanges--item middle-xs">
+              <div className="exchanges--item__logo">
+                <LazyLoad height={40}>
+                  <img src={x.img} alt="img" />
+                </LazyLoad>
+              </div>
+              <div className="exchanges--item__name">
+                <h4>{x.title}</h4>
+                <span>{x.link}</span>
+              </div>
             </div>
-            <div className="exchanges--item__name">
-              <h4>{x.title}</h4>
-              <span>{x.link}</span>
-            </div>
-          </div>
+          </a>
         </a>
-      </a>
-    </div>
-  ));
+      </div>
+    )
+  });
 
   return (
     <div className="row start-sm">
