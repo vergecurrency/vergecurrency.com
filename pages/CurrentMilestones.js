68798416cb101c6fbeb74872a21c4ac9929932fa
justinvforvendetta <justinvforvendetta@gmail.com>
2023-06-19 21:22:35 -0400

    update milestone page
    see title of commit for more information ;p

--- pages/CurrentMilestones.js
+++ pages/CurrentMilestones.js
@@ -687,6 +687,266 @@ const mileStones = [
     infoLabel: 'Binance Earn Page',
     infoUrl: 'https://www.binance.com/en/earn/xvg'
   }
+  {
+    done: true,
+    doneDate: '19th July 2022',
+    title: 'Partnership Announcement',
+    description: 'Verge partners with 7b crypto broker',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/7bbroker/status/1549380733105586177?t=VMGtO3qzhl1zhRemARpJhQ&s=19'
+  }
+  {
+    done: true,
+    doneDate: '23rd August 2022',
+    title: 'Fueled by Verge Rider at the FIM Junior World Motocross Championship',
+    description: 'Jayden Riley, competes in Finland at the World Motocross Championship',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/Fueled_by_Verge/status/1562138431815237632?t=2iikd2XQKtyx87gDz2_Qsw&s=19'
+  }
+  {
+    done: true,
+    doneDate: '1st September 2022',
+    title: 'Fresh Designs in the Merch Store',
+    description: 'New designs available at vergecurrency.store',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/vergecurrency/status/1565308153268473857?t=8S2_cr-CE6xyaKh-8ncP9A&s=19'
+  }
+  {
+    done: true,
+    doneDate: '3rd September 2022',
+    title: 'Fueled by Verge Team Trailer Road Trip',
+    description: 'The FBV Team Trailer travelled through 9 states, bringing awareness to $XVG',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/Fueled_by_Verge/status/1566088857292255232?t=PdcfC5rUq9CPBae7vwR33w&s=19'
+  }
+  {
+    done: true,
+    doneDate: '4th October 2022',
+    title: 'Buy XVG on Guardarian in the USA',
+    description: 'You can now buy XVG directly in the USA on Guardarian',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Exchange Link',
+    infoUrl: 'https://guardarian.com/buy-xvg'
+  }
+  {
+    done: true,
+    doneDate: '8th October 2022',
+    title: 'Gil Linster Celebrates His First Career Podium Finish',
+    description: 'Gil celebrates his first Euro Nascar  podium finish in the Verge car.',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/GilLinster/status/1578819252249055233?t=7qCHJm0BTkwfZqVbtqWlKQ&s=19'
+  }
+  {
+    done: true,
+    doneDate: '9th October 2022',
+    title: 'Verge celebrates it's 8th Birthday',
+    description: 'Founded on 9th October 2014, we celebrate our 8th birthday!',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/vergecurrency/status/1579138979940929536?t=JhoYlb8HuO-zUxv4ZEeUMQ&s=19'
+  }
+  {
+    done: true,
+    doneDate: '17th October 2022',
+    title: 'Verge Accepted on Shopware!',
+    description: 'In collaboration with our partners Now payments, XVG can now be accepted on Shopware!',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Exchange Link',
+    infoUrl: 'https://nowpayments.io/payment-integration/shopware-plugin'
+  }
+  {
+    done: true,
+    doneDate: '28th October 2022',
+    title: 'Send and Receive XVG with Verge Domain Names!',
+    description: 'We are happy to announce integration on Unstoppable Web3 domains to send & receive XVG!',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Exchange Link',
+    infoUrl: 'https://medium.com/vergecurrency/unstoppable-verge-8c6b1d8ed1a1'
+  }
+  {
+    done: true,
+    doneDate: '17th November 2022',
+    title: 'XVG Integrated into NOW Wallet',
+    description: '@NOW_Wallet , a non-custodial wallet by ChangeNOW.io, has added support for XVG.',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Exchange Link',
+    infoUrl: 'https://twitter.com/vergecurrency/status/1593282120872767498?t=HMat8lieXCcF4msvfUMpqA&s=19'
+  }
+  {
+    done: true,
+    doneDate: '22nd December 2022',
+    title: 'New Pairing on Huobi Global',
+    description: 'Huobi Global has listed the following pairing: $XVG/ $USDT',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/vergecurrency/status/1605946461174509568?t=ch0H4-DRp8FzEsjPJsuEUA&s=19'
+  }
+  {
+    done: true,
+    doneDate: '29th December 2022',
+    title: 'Fueled by Verge',
+    description: 'The Verge Currency  "Mighty Pucks" finished with the SILVER MEDAL at the 2022 Tim Hortons WC Pond Hockey tournament',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/vergecurrency/status/1605946461174509568?t=ch0H4-DRp8FzEsjPJsuEUA&s=19'
+  }
+  {
+    done: true,
+    doneDate: '29th December 2022',
+    title: 'New Exchange Listing!',
+    description: 'Verge is listed on Swapika.com, instant crypto exchange',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/Swapika_com/status/1618332436227817472?t=DVH7MOeYcCAHGxvG8eto_g&s=19'
+  }
+  {
+    done: true,
+    doneDate: '2nd February 2023',
+    title: 'New listing: StealthEx Mobile App',
+    description: 'XVG is now available for exchange on StealthEX Mobile App',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/StealthEX_io/status/1621134537790201856?t=kFoVWzwFzcg1jxKBZ_llxw&s=19'
+  }
+  {
+    done: true,
+    doneDate: '6th February 2023',
+    title: 'New listing: CoinRabbit',
+    description: 'XVG is now listed on CoinRabbit!',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/CoinRabbitLoans/status/1622581947196121090?t=KDtIjQiWaK1my_L5xCtE9Q&s=19'
+  }
+  {
+    done: true,
+    doneDate: '8th February 2023',
+    title: 'New listing: Exolix',
+    description: 'Verge Currency has been listed on exolix.com',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/vergecurrency/status/1623306062974656513?t=qy4JsUwao3oxwZRL8bIEmg&s=19'
+  }
+  {
+    done: true,
+    doneDate: '10th February 2023',
+    title: 'Partnership Announcement:',
+    description: 'NOWNodes.io, provider of full nodes and block explorers, partners with Verge Currency',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/NOWNodes/status/1624061852463378451?t=63vqOnoAf7Zx_UKVWrtMfA&s=19'
+  }
+  {
+    done: true,
+    doneDate: '17th February 2023',
+    title: 'Congratulations Gil Linster!',
+    description: 'US Embassy Luxembourg sends a message congratulating XVG, Fueled by Verge, Voice life NASCAR driver Gil Linster',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/vergecurrency/status/1626647460910354433?t=M9GcA_78FloV4vItI_FrSQ&s=19'
+  }
+  {
+    done: true,
+    doneDate: '4th April 2023',
+    title: 'Partnership Announcement - Web3 Berlin',
+    description: 'The biggest crypto and NFT conference in Europe',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/berlinweb3com/status/1643247260564594690?t=SstNO6UE6q88k4ruiYI6jw&s=19'
+  }
+  {
+    done: true,
+    doneDate: '11th April 2023',
+    title: 'XVG is Listed on SuperEx exchange',
+    description: 'SuperEx is located in the Philippines and is the First Web 3.0 Crypto Exchange',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/vergecurrency/status/1645773478208090113?t=bW7eWHrd8o_dFroisEA_9w&s=19'
+  }
+  {
+    done: true,
+    doneDate: '13th April 2023',
+    title: 'Verge Join Forces with Cryptowallet.com',
+    description: 'Buy, sell and manage XVG on cryptowallet.com',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/vergecurrency/status/1645773478208090113?t=bW7eWHrd8o_dFroisEA_9w&s=19'
+  }
+  {
+    done: true,
+    doneDate: '23rd April 2023',
+    title: 'Verge Motocross Riders Race at the Spring Super Series',
+    description: 'Danny and Wyatt race at the Wild Rose MX Spring Super Series',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/Fueled_by_Verge/status/1650168309546123267?t=nrPOVX3tIejFniZOX1b2xg&s=19'
+  }
+  {
+    done: true,
+    doneDate: '29th April 2023',
+    title: 'Verge and Voice Life Attend Crypto V Summit in Dubai!',
+    description: 'Mihael Radoslovic who is the COO at Voice Life spoke about Verge Currency, Blockchain, NFTs and Voice Life tech',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/itsmihael/status/1652299090863046656?t=4ZAqZzbeMAxbfriwhelo2A&s=19'
+  }
+  {
+    done: true,
+    doneDate: '23rd May 2023',
+    title: 'The Official Verge XVGApes NFT Collection Goes Live',
+    description: 'XVG Apes have arrived! Verge Ambassador @Binary_Punks and Verge Currency launch their official XVGApes collection',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/vergecurrency/status/1661063689477275671?t=PPjkWnmw-22WN7dRunb8zw&s=19'
+  }
+  {
+    done: true,
+    doneDate: '8th June 2023',
+    title: 'Verge and Voice Life Speak at the Web3 Berlin Conference',
+    description: 'Verge and Voice Life share a booth at the biggest crypto and NFT conference in Europe',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://twitter.com/itsmihael/status/1666785358749605889?t=Q4oBHnAZkny787CQnOIdoA&s=19'
+  }
+  {
+    done: true,
+    doneDate: '5th June 2023',
+    title: 'Verge is Now ISO 20022 Compliant',
+    description: 'Bridging the gap between traditional finance & cryptocurrency.',
+    progress: 100,
+    progressState: '',
+    infoLabel: 'Twitter Link',
+    infoUrl: 'https://medium.com/vergecurrency/iso-20022-verge-compliance-dcc67dc3e933'
+  }
   // {
   //   done: true or false,
   //   doneDate: 'Relevant Date',
