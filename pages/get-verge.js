bc0367fa874cb8978954d1c0424d27c9c5fa39da
sunerok <justinvforvendetta@gmail.com>
2024-11-11 19:09:21 -0500

    move widget

--- pages/get-verge.js
+++ pages/get-verge.js
@@ -13,9 +13,27 @@ function GetVerge(props) {
       <Head>
         <title key="title">{t('common:meta.get-verge.title', { defaultValue: 'Get Verge - VergeCurrency.com' })}</title>
       </Head>
+      <div className="get-verge pt-large pb">
+        <div className="container">
+          <div className="row center-xs middle-xs pt pb">
+            <p className="mb">
+                <a
+                  href="https://coinmarketcap.com/currencies/verge/"
+                  rel="noopener noreferrer"
+                >
+                  {t('get-verge:body.intro3', { defaultValue: 'Visit Coinmarketcap for an additional list of market pairs.' })}
+                </a>
+              </p>
 
-      {/* Add the iframe here */}
-      <div className="iframe-container" style={{ textAlign: 'center' }}>
+              <h2>{t('get-verge:body.intro', { defaultValue: 'Trade Verge on the most popular exchanges.' })}</h2>
+              <h3>{t('get-verge:body.intro2', { defaultValue: 'We’re proud to be listed on:' })}</h3>
+
+              <Exchanges />
+
+              <span className="hidden-xs">
+                <br />
+              </span>
+			  <div className="iframe-container" style={{ textAlign: 'center' }}>
         <iframe 
           src="https://swapspace.co/widget/e690ba97ac5620dc27d4101c" 
           frameBorder="0" 
@@ -29,48 +47,6 @@ function GetVerge(props) {
           }} 
         />
       </div>
-
-      <div className="get-verge pt-large pb">
-        <div className="container">
-          <div className="row center-xs middle-xs pt pb">
-            <p className="mb">
-                <a
-                  href="https://coinmarketcap.com/currencies/verge/"
-                  rel="noopener noreferrer"
-                >
-                  {t('get-verge:body.intro3', { defaultValue: 'Visit Coinmarketcap for an additional list of market pairs.' })}
-                </a>
-              </p>
-            {/* <div className="col-xs-10">
-              <h1>{t('get-verge:body.title', { defaultValue: 'Get Verge' })}</h1>
-              <br />
-            </div> */}
-            {/* <div className="col-xs-12 pb-large">
-              <h2>{t('get-verge:body.direct', { defaultValue: 'Check out the VergeCurrency.Exchange, powered by ChangeNOW.' })}</h2>
-              <br />
-              <a target="_blank" href="https://vergecurrency.exchange" className="img-link-hover">
-                <img src="/static/img/verge-exchange.png" alt="VergeCurrency.Exchange" alt="VergeCurrency Exchange" />
-              </a>
-            </div> */}
-            <div className="col-xs-10">
-              {/* <h2>{t('get-verge:body.fiat', { defaultValue: 'Buy Verge (XVG) directly with fiat currency.' })}</h2>
-              <h3>{t('get-verge:body.fiat2', { defaultValue: 'To find out more, please visit:' })}</h3>
-              <Link href="https://verge.zendesk.com/hc/en-us/sections/360003859972-Where-How-Can-I-Buy-Verge-XVG-">
-                <a target="_blank" rel="noopener noreferrer" href="https://verge.zendesk.com/hc/en-us/sections/360003859972-Where-How-Can-I-Buy-Verge-XVG-">
-                  <font color="black">Verge Zendesk - Getting Started</font>
-                </a>
-              </Link>
-              <br />
-              <br />
-              <br /> */}
-              <h2>{t('get-verge:body.intro', { defaultValue: 'Trade Verge on the most popular exchanges.' })}</h2>
-              <h3>{t('get-verge:body.intro2', { defaultValue: 'We’re proud to be listed on:' })}</h3>
-
-              <Exchanges />
-
-              <span className="hidden-xs">
-                <br />
-              </span>
               <h3>
                 {t('home:getverge.text6', {defaultValue: 'Exchange Disclaimer:',})}
                 {' '}
