6044e854bd5271a4f29df7da0bf6ab103514c951
sunerok <justinvforvendetta@gmail.com>
2026-04-11 17:30:43 -0400

    Add minimal privacy FAQ page

--- pages/faq/privacy.js
+++ pages/faq/privacy.js
@@ -3,30 +3,17 @@ import Head from 'next/head';
 import Layout from '../../components/Layout';
 import Content from '../../components/resources/Content';
 
-import { translate } from 'react-i18next';
-import i18n from '../../i18n';
-
-function FAQ_privacy(props) {
-  const { t } = props;
-
+function FAQPrivacy() {
   return (
     <Layout>
       <Head>
-        <title key="title">{t('common:meta.faq.privacy.title', { defaultValue: 'FAQ - Privacy - VergeCurrency.com' })}</title>
+        <title key="title">Privacy - VergeCurrency.com</title>
       </Head>
       <div className="learnmore">
         <div className="container">
           <Content>
             <div className="faq">
-              <h1>{t('faq:privacy:subheader', { defaultValue: 'Privacy' })}</h1> 
-              <h2>{t('faq:privacy:vergeanon:question')}</h2>
-              <p dangerouslySetInnerHTML={{ __html: t('faq:privacy:vergeanon:answer') }} />
-              <h2>{t('faq:privacy:whytor:question')}</h2>
-              <p dangerouslySetInnerHTML={{ __html: t('faq:privacy:whytor:answer') }} />
-              <h2>{t('faq:privacy:vergemasternode:question')}</h2>
-              <p dangerouslySetInnerHTML={{ __html: t('faq:privacy:vergemasternode:answer') }} />
-              <h2>{t('faq:privacy:torandroidnoconnection:question')}</h2>
-              <p dangerouslySetInnerHTML={{ __html: t('faq:privacy:torandroidnoconnection:answer') }} />
+              <p>we do not collect your data</p>
             </div>
           </Content>
         </div>
@@ -35,6 +22,4 @@ function FAQ_privacy(props) {
   );
 }
 
-const Extended = translate(['faq', 'common'], { i18n })(FAQ_privacy);
-
-export default Extended;
+export default FAQPrivacy;
