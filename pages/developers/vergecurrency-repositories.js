edb7373da275f2eac3d090a894899ee00b109743
sunerok <justinvforvendetta@gmail.com>
2026-08-02 00:51:41 -0400

    update repository names/links

--- pages/developers/vergecurrency-repositories.js
+++ pages/developers/vergecurrency-repositories.js
@@ -26,17 +26,15 @@ function VergeCurrency_Repositories(props) {
               <a href="https://github.com/vergecurrency/VERGE" target="_blank" rel="noopener noreferrer">https://github.com/vergecurrency/verge</a>
 			  <h2>{t('verge-repos:website', { defaultValue: 'Verge Website' })}</h2>
               <a href="https://github.com/vergecurrency/vergecurrency.com" target="_blank" rel="noopener noreferrer">https://github.com/vergecurrency/vergecurrency.com</a>
-			  <h2>{t('verge-repos:core-installers', { defaultValue: 'Verge Android Wallet' })}</h2>
-              <a href="https://github.com/vergecurrency/tordroid" target="_blank" rel="noopener noreferrer">https://github.com/vergecurrency/tordroid</a>
               <h2>{t('verge-repos:blackpaper', { defaultValue: 'Verge iOS Wallet' })}</h2>
               <a href="https://github.com/vergecurrency/vIOS" target="_blank" rel="noopener noreferrer">https://github.com/vergecurrency/vIOS</a>
-              <h2>{t('verge-repos:electrum-via-tor', { defaultValue: 'Verge Android Wallet' })}</h2>
-              <a href="https://github.com/vergecurrency/electrum-xvg-tor" target="_blank" rel="noopener noreferrer">https://github.com/vergecurrency/electrum-xvg-tor</a>
+              <h2>{t('verge-repos:electrum-via-tor', { defaultValue: 'Verge Android/Meta VR Wallet (Verge XVR)' })}</h2>
+              <a href="https://github.com/vergecurrency/vergexvr target="_blank" rel="noopener noreferrer">https://github.com/vergecurrency/vergexvr</a>
               <h2>{t('verge-repos:graphics', { defaultValue: 'Verge Graphics' })}</h2>
               <a href="https://github.com/vergecurrency/verge-graphics" target="_blank" rel="noopener noreferrer">https://github.com/vergecurrency/verge-graphics</a>
               <h2>{t('verge-repos:vendor-integration', { defaultValue: 'Verge libraries for Node.js, Ruby, PHP, Python, and Coinlib (Flutter)' })}</h2>
               <a href="https://github.com/vergecurrency/nodejs-verge" target="_blank" rel="noopener noreferrer">https://github.com/vergecurrency/nodejs-verge</a><br />
-              <a href="https://github.com/vergecurrency/verge-ruby" target="_blank" rel="noopener noreferrer">https://github.com/vergecurrency/verge-ruby</a><br />
+              <a href="https://github.com/vergecurrency/verge-ruby-client" target="_blank" rel="noopener noreferrer">https://github.com/vergecurrency/verge-ruby-client</a><br />
 			  <a href="https://github.com/vergecurrency/php-verge" target="_blank" rel="noopener noreferrer">https://github.com/vergecurrency/php-verge</a><br />
               <a href="https://github.com/vergecurrency/verge-python" target="_blank" rel="noopener noreferrer">https://github.com/vergecurrency/verge-python</a><br />
 			  <a href="https://github.com/vergecurrency/coinlib" target="_blank" rel="noopener noreferrer">https://github.com/vergecurrency/coinlib</a>
